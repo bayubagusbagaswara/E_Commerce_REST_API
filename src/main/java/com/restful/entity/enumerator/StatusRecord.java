17f7b40c2674efa56e94ef5c671df3150982a56d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-21 10:00:04 +0700

    add enum StatusRecord

---
+++ src/main/java/com/restful/entity/enumerator/StatusRecord.java
@@ -0,0 +1,16 @@
+package com.restful.entity.enumerator;
+
+public enum StatusRecord {
+    ACTIVE("ACTIVE"),
+    INACTIVE("INACTIVE");
+
+    private final String statusRecord;
+
+    StatusRecord(String statusRecord) {
+        this.statusRecord = statusRecord;
+    }
+
+    public String getStatusRecord() {
+        return statusRecord;
+    }
+}
