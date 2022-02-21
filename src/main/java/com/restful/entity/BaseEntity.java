2a4064732a961c6f9d48ffeb392297851f6e27d5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-21 09:46:38 +0700

    add entity BaseEntity

---
+++ src/main/java/com/restful/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package com.restful.entity;
+
+import com.restful.entity.enumerator.StatusRecord;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    @Column(name = "id", nullable = false, length = 64)
+    private String id;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_record", nullable = false)
+    private StatusRecord statusRecord = StatusRecord.ACTIVE;
+}
