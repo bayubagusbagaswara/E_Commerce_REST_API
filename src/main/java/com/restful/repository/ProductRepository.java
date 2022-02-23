6157f455f84bee6cc859609ec54c11889c8b9d3e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-23 19:14:48 +0700

    add repository layer ProductRepository

---
+++ src/main/java/com/restful/repository/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.restful.repository;
+
+import com.restful.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+    // get product by name containing ignore case
+    List<Product> findAllByNameContainingIgnoreCase(String name);
+
+    // get all product sorting by price
+    List<Product> findAllAndOrderByPrice();
+
+    // get product by price between
+    List<Product> findAllByPriceBetween(BigDecimal priceMin, BigDecimal priceMax);
+
+    // get product by name containing and price between
+    List<Product> findAllByNameContainingAndPriceBetween(String name, BigDecimal priceMin, BigDecimal priceMax);
+
+    // get product by category id
+    List<Product> findAllByCategoryId(String categoryId);
+
+    // get product by supplier id
+    List<Product> findAllBySuppliersId(String supplierId);
+}
