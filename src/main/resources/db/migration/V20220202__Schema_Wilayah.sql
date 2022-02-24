e5188fe4d3cfe8a386fca7c7ba4563afdcb7ab8f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-24 20:21:02 +0700

    add schema migrate wilayah

---
+++ src/main/resources/db/migration/V20220202__Schema_Wilayah.sql
@@ -0,0 +1,72 @@
+
+-- PROVINSI
+create table provinsi (
+    id character varying(64) not null primary key default uuid_generate_v4(),
+    created_at timestamp not null default now(),
+    created_by character varying(255) not null,
+    status_record character varying(255) not null,
+    updated_at timestamp without time zone,
+    updated_by character varying(255),
+    code character varying(255) not null,
+    name character varying(255) not null
+);
+
+alter table provinsi
+    add constraint provinsi_unique_code unique (code);
+
+-- KOTA
+create table kota (
+    id character varying(64) not null primary key default uuid_generate_v4(),
+    created_at timestamp not null default now(),
+    created_by character varying(255) not null,
+    status_record character varying(255) not null,
+    updated_at timestamp without time zone,
+    updated_by character varying(255),
+    code character varying(255) not null,
+    name character varying(255) not null,
+    id_provinsi character varying(64)
+);
+
+alter table kota
+    add constraint kota_unique_code unique (code);
+alter table kota
+    add constraint fk_kota_provinsi foreign key (id_provinsi) references provinsi(id);
+
+-- KECAMATAN
+create table kecamatan (
+    id character varying(64) not null primary key default uuid_generate_v4(),
+    created_at timestamp not null default now(),
+    created_by character varying(255) not null,
+    status_record character varying(255) not null,
+    updated_at timestamp without time zone,
+    updated_by character varying(255),
+    code character varying(255) not null,
+    name character varying(255) not null,
+    id_kota character varying(64)
+);
+
+alter table kecamatan
+    add constraint kecamatan_unique_code unique (code);
+alter table kecamatan
+    add constraint fk_kecamatan_kota foreign key (id_kota) references kota(id);
+
+-- KELURAHAN
+create table kelurahan (
+    id character varying(64) not null primary key default uuid_generate_v4(),
+    created_at timestamp not null default now(),
+    created_by character varying(255) not null,
+    status_record character varying(255) not null,
+    updated_at timestamp without time zone,
+    updated_by character varying(255),
+    code character varying(255) not null,
+    name character varying(255) not null,
+    postal_code character varying(255) not null,
+    id_kecamatan character varying(64)
+);
+
+alter table kelurahan
+    add constraint kelurahan_unique_code unique (code);
+alter table kelurahan
+    add constraint kelurahan_unique_postal_code unique (postal_code);
+alter table kelurahan
+    add constraint fk_kelurahan_kecamatan foreign key (id_kecamatan) references kecamatan(id);
