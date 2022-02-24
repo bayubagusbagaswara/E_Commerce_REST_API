227968b4ba5bdc3f14a57c3a7d2192ab194f8494
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-24 19:48:21 +0700

    update entity ProductDetail

--- src/main/java/com/restful/entity/ProductDetail.java
+++ src/main/java/com/restful/entity/ProductDetail.java
@@ -8,9 +8,12 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "product_detail")
+@Table(name = "product_detail", uniqueConstraints = {
+        @UniqueConstraint(name = "product_detail_unique_sku", columnNames = "SKU")
+})
 @Getter
 @Setter
 @AllArgsConstructor
