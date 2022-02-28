7c3221671d0dba125596fbd041e520641c4fdb9d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-28 11:07:18 +0700

    add method contract createProduct in ProductService

---
+++ src/main/java/com/restful/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.restful.service;
+
+import com.restful.dto.product.CreateProductRequestDto;
+
+public interface ProductService {
+
+    // create new product
+    CreateProductRequestDto createProduct(CreateProductRequestDto createProductRequestDto);
+
+    // get product by id
+
+    // list all product
+
+    // update product by id
+
+    // delete product by id
+
+    // get product by name
+
+    // get all product by category id
+}
