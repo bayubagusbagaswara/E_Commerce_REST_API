84e8bdbce8a283300e84e962ce9c0f92a94106a0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-28 10:15:22 +0700

    add dto CreateProductDetailRequestDto

---
+++ src/main/java/com/restful/dto/productdetail/CreateProductDetailRequestDto.java
@@ -0,0 +1,23 @@
+package com.restful.dto.productdetail;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateProductDetailRequestDto {
+
+    @NotBlank(message = "SKU must not be blank")
+    @Size(max = 50, message = "SKU maximum length is 50 characters")
+    private String sku;
+
+    @NotBlank(message = "Description must not be blank")
+    @Size(max = 500, message = "Description maximum length is 500 characters")
+    private String description;
+}
