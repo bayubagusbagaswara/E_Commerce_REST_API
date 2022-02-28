dee2b491e7935e74b13d9370148596c87dbff6c8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-28 10:20:34 +0700

    add dto CreateCategoryRequestDto

---
+++ src/main/java/com/restful/dto/category/CreateCategoryRequestDto.java
@@ -0,0 +1,24 @@
+package com.restful.dto.category;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateCategoryRequestDto {
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(max = 50, message = "Name length maximum is 50 characters")
+    private String name;
+
+    @NotBlank(message = "Description must not be blank")
+    @Max(value = 500, message = "Description length maximum is 500 characters")
+    private String description;
+}
