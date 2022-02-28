0ee12298385606e8c1e2da3ba463a3b3a453a92e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-28 10:36:34 +0700

    add dto CreateSupplierRequestDto

---
+++ src/main/java/com/restful/dto/supplier/CreateSupplierRequestDto.java
@@ -0,0 +1,35 @@
+package com.restful.dto.supplier;
+
+import com.restful.entity.Address;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateSupplierRequestDto {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String gender;
+
+    @NotBlank
+    private String mobilePhone;
+
+    @NotBlank
+    private String kelurahanId;
+
+    @NotBlank
+    private String street;
+}
