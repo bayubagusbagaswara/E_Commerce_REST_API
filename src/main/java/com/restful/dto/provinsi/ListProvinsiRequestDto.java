e02e3e77783b353b4b74fab36d16cf3bded38002
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-05 16:45:58 +0700

    add dto ListProvinsiRequestDto

---
+++ src/main/java/com/restful/dto/provinsi/ListProvinsiRequestDto.java
@@ -0,0 +1,28 @@
+package com.restful.dto.provinsi;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListProvinsiRequestDto {
+
+    @NotNull(message = "Page Index can not null")
+    @Min(value = 0, message = "Page Index must not be less than zero")
+    private Integer pageNo;
+
+    @NotNull(message = "Page Size can not null")
+    @Min(value = 1, message = "Page Size must not be less than one")
+    private Integer pageSize;
+
+    @NotBlank(message = "Sort by field must not be blank")
+    private String sortBy;
+
+    @NotBlank(message = "Sort direction must be asc or desc")
+    private String sortDir;
+}
