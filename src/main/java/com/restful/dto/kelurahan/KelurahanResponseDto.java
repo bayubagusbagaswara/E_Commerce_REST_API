6efa83df6b52d1d9abcd2420b8e75e06d2f85791
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-05 16:54:16 +0700

    update dto KelurahanResponseDto

--- src/main/java/com/restful/dto/kelurahan/KelurahanResponseDto.java
+++ src/main/java/com/restful/dto/kelurahan/KelurahanResponseDto.java
@@ -1,28 +1,23 @@
 package com.restful.dto.kelurahan;
 
+import com.restful.dto.kecamatan.KecamatanResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class KelurahanResponseDto {
 
     private String id;
-
-    private String code;
     private String name;
-
+    private String code;
     private String postalCode;
-
-//    private KecamatanResponseDto kecamatanResponseDto;
-
     private LocalDateTime createdAt;
-    private LocalDate updatedAt;
+    private LocalDateTime updatedAt;
+    private KecamatanResponseDto kecamatanResponseDto;
 }
