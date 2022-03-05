3b2b0c6494e7b78c5047e1105cdda89447a16a4e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-05 19:26:27 +0700

    update dto CreateProductRequestDto

--- src/main/java/com/restful/dto/product/CreateProductRequestDto.java
+++ src/main/java/com/restful/dto/product/CreateProductRequestDto.java
@@ -1,35 +1,37 @@
 package com.restful.dto.product;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateProductRequestDto {
 
-    @NotBlank(message = "Name must not be blank")
+    @NotBlank(message = "Product name must not be blank")
     private String name;
 
-    @NotNull(message = "Price can not null")
-    @Size(min = 1, message = "Price must be minimum 1")
+    @NotBlank(message = "Product SKU must not be blank")
+    @Size(max = 50, message = "Product SKU maximum length is 50 characters")
+    private String sku;
+
+    @NotNull(message = "Product price can not null")
+    @Size(min = 1, message = "Product price must be minimum 1")
     private BigDecimal price;
 
-    @NotNull(message = "Quantity can not null")
-    @Size(min = 0, message = "Quantity must be minimum 0")
+    @NotNull(message = "Product quantity can not null")
+    @Size(min = 0, message = "Product quantity must be minimum 0")
     private Integer quantity;
 
-    @NotBlank(message = "Product Detail ID must not be blank")
-    private String productDetailId;
+    @NotBlank(message = "Product description must not be blank")
+    @Size(max = 500, message = "Product description maximum length is 500 characters")
+    private String description;
 
     @NotBlank(message = "Category ID must not be blank")
     private String categoryId;
