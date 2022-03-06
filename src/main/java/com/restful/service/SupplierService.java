d0d7154057e92b0e7eb6a879e727d272d59fadc0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-06 15:04:39 +0700

    add method contract service SupplierService

---
+++ src/main/java/com/restful/service/SupplierService.java
@@ -0,0 +1,25 @@
+package com.restful.service;
+
+public interface SupplierService {
+
+    // create supplier
+    // user baru harus mengirimkan data:
+    // name, email, mobile phone, gender, address
+
+    // get supplier by id
+
+    // get all supplier
+
+    // update supplier
+
+    // delete supplier
+
+    // get supplier by name
+
+    // get supplier by email
+
+    // get supplier by product
+    // jadi product A memiliki supplier siapa saja
+
+    // add product to supplier
+}
