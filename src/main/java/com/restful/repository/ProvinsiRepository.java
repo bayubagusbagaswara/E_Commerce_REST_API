3c8bab4473d592807f0b6e158d77d604280b868c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 19:00:04 +0700

    update method repository ProvinsiRepository

--- src/main/java/com/restful/repository/ProvinsiRepository.java
+++ src/main/java/com/restful/repository/ProvinsiRepository.java
@@ -3,11 +3,17 @@ package com.restful.repository;
 import com.restful.entity.address.Provinsi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProvinsiRepository extends JpaRepository<Provinsi, String> {
 
     // get provinsi by code
+    Optional<Provinsi> findAllByCode(String code);
 
     // get provinsi by name ignore case
+    Optional<Provinsi> findAllByNameIgnoreCase(String name);
 
     // get provinsi by name containing ignore case
+    List<Provinsi> findAllByNameContainingIgnoreCase(String name);
 }
