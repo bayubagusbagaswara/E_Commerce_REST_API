757a2b4736a9ae34aaecafc979d97acb7607c26c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 16:22:17 +0700

    update method service AddressService

--- src/main/java/com/restful/service/AddressService.java
+++ src/main/java/com/restful/service/AddressService.java
@@ -3,4 +3,9 @@ package com.restful.service;
 public interface AddressService {
 
     // create address
+    // ini butuh supplier id
+    // karena address ini berelasi dengan supplier
+
+    // update address
+    // ini juga supplier id
 }
