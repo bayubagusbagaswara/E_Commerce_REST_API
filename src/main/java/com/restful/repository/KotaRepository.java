0ce4ca970b3d360cad686b746281cc934e9c221c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 19:01:00 +0700

    update method repository KotaRepository

--- src/main/java/com/restful/repository/KotaRepository.java
+++ src/main/java/com/restful/repository/KotaRepository.java
@@ -3,9 +3,12 @@ package com.restful.repository;
 import com.restful.entity.address.Kota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface KotaRepository extends JpaRepository<Kota, String> {
 
-    // get kota by name ignore case
-    // get kota by code
-    // get kota by name containing
+    Optional<Kota> findAllByCode(String code);
+    Optional<Kota> findAllByNameIgnoreCase(String name);
+    List<Kota> findAllByNameContainingIgnoreCase(String name);
 }
