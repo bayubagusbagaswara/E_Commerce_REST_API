4e184ee857041a76e4847fd48ac77bed8d282710
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 16:34:52 +0700

    add field postal code to entity Address

--- src/main/java/com/restful/entity/Address.java
+++ src/main/java/com/restful/entity/Address.java
@@ -9,7 +9,9 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "address")
+@Table(name = "address", uniqueConstraints = {
+        @UniqueConstraint(name = "address_unique_postal_code", columnNames = "postal_code")
+})
 @Getter
 @Setter
 @AllArgsConstructor
@@ -19,7 +21,10 @@ public class Address extends BaseEntity {
     @Column(name = "street", length = 100)
     private String street;
 
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
     @OneToOne
-    @JoinColumn(name = "id_kelurahan", foreignKey = @ForeignKey(name = "fk_kelurahan"))
+    @JoinColumn(name = "id_kelurahan", foreignKey = @ForeignKey(name = "fk_address_kelurahan"))
     private Kelurahan kelurahan;
 }
