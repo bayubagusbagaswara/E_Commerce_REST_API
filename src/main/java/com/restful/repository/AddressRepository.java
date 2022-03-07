0b3c69d61eec65addaae0415bbe05c269405e461
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 18:59:48 +0700

    update method repository AddressRepository

--- src/main/java/com/restful/repository/AddressRepository.java
+++ src/main/java/com/restful/repository/AddressRepository.java
@@ -3,5 +3,10 @@ package com.restful.repository;
 import com.restful.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AddressRepository extends JpaRepository<Address, String> {
+
+    // find by postal code
+    Optional<Address> findAllByPostalCode(String postalCode);
 }
