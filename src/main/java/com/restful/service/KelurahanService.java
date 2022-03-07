876875801c708521829fbcb87aaed652dc319964
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 16:21:29 +0700

    update method service KelurahanService

--- src/main/java/com/restful/service/KelurahanService.java
+++ src/main/java/com/restful/service/KelurahanService.java
@@ -1,4 +1,24 @@
 package com.restful.service;
 
 public interface KelurahanService {
+
+    // create kelurahan
+
+    // get kelurahan by id
+
+    // get all kelurahan
+
+    // update kelurahan
+
+    // delete kelurahan
+
+    // get all kelurahan
+
+    // get kelurahan by postal code
+
+    // get kelurahan by name contains
+
+    // get kelurahan by code
+
+    // get kelurahan by kecamatan id, return is List of Kelurahan
 }
