8c26a048e4e0cfae036def0586745ff5350741c7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 16:20:41 +0700

    update method service ProvinsiService

--- src/main/java/com/restful/service/ProvinsiService.java
+++ src/main/java/com/restful/service/ProvinsiService.java
@@ -1,4 +1,18 @@
 package com.restful.service;
 
 public interface ProvinsiService {
+
+    // create provinsi
+
+    // get provinsi by id
+
+    // get all provinsi
+
+    // update provinsi
+
+    // delete provinsi
+
+    // get provinsi by name contains
+
+    // get provinsi by code
 }
