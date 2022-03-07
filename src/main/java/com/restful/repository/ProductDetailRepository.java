ff91edf4e2001e4108d95b62f9289b9e6eff68f2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 19:02:10 +0700

    update method repository ProductDetailRepository

--- src/main/java/com/restful/repository/ProductDetailRepository.java
+++ src/main/java/com/restful/repository/ProductDetailRepository.java
@@ -3,7 +3,10 @@ package com.restful.repository;
 import com.restful.entity.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, String> {
 
     // get product detail by SKU
+    Optional<ProductDetail> findAllBySku(String sku);
 }
