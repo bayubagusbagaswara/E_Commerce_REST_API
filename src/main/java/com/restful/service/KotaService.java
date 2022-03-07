03235fe96e0325e1236055fced27766f27ebb04e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 16:20:57 +0700

    update method service KotaService

--- src/main/java/com/restful/service/KotaService.java
+++ src/main/java/com/restful/service/KotaService.java
@@ -1,4 +1,20 @@
 package com.restful.service;
 
 public interface KotaService {
+
+    // create kota
+
+    // get kota by id
+
+    // get all kota
+
+    // update kota
+
+    // delete kota by id
+
+    // get kota by name contains
+
+    // get kota by code
+
+    // get kota by provinsi id, balikan adalah List of Kota
 }
