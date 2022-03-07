8e801a64cdca8edc3e4493ad9a30d6838c0134dc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 19:01:53 +0700

    update method repository SupplierRepository

--- src/main/java/com/restful/repository/SupplierRepository.java
+++ src/main/java/com/restful/repository/SupplierRepository.java
@@ -3,10 +3,20 @@ package com.restful.repository;
 import com.restful.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
 
     // get supplier by name ignore case
+    Optional<Supplier> findAllByNameIgnoreCase(String name);
+
     // get supplier by email
+    Optional<Supplier> findAllByEmail(String email);
+
     // get all supplier by name containing ignore case
+    List<Supplier> findAllByNameContainingIgnoreCase(String name);
+
     // get all supplier by product id
+    List<Supplier> findAllByProductsId(String productId);
 }
