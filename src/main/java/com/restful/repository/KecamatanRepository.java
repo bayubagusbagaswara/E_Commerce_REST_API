7a27c4714917cd370eee6efd65a0591a92c6be70
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 19:01:20 +0700

    update method repository KecamatanRepository

--- src/main/java/com/restful/repository/KecamatanRepository.java
+++ src/main/java/com/restful/repository/KecamatanRepository.java
@@ -3,5 +3,12 @@ package com.restful.repository;
 import com.restful.entity.address.Kecamatan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface KecamatanRepository extends JpaRepository<Kecamatan, String> {
+
+    Optional<Kecamatan> findAllByCode(String code);
+    Optional<Kecamatan> findAllByNameIgnoreCase(String name);
+    List<Kecamatan> findAllByNameContainingIgnoreCase(String name);
 }
