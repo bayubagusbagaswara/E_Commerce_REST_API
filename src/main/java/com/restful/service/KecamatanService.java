2c325683dc8a170051c330f460d7e3621c644d22
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-07 16:21:08 +0700

    update method service KecamatanService

--- src/main/java/com/restful/service/KecamatanService.java
+++ src/main/java/com/restful/service/KecamatanService.java
@@ -1,4 +1,20 @@
 package com.restful.service;
 
 public interface KecamatanService {
+
+    // create kecamatan
+
+    // get kecamatan by id
+
+    // get all kecamatan
+
+    // update kecamatan
+
+    // delete kecamatan
+
+    // get kecamatan by name contains
+
+    // get kecamatan by code
+
+    // get kecamatan by kota id, return is List of Kecamatan
 }
