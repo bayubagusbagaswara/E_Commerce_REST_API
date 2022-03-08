f24153f3a3a817652f317111e8fb0699a7b1ad20
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 07:06:32 +0700

    update method contract service KecamatanService

--- src/main/java/com/restful/service/KecamatanService.java
+++ src/main/java/com/restful/service/KecamatanService.java
@@ -1,20 +1,36 @@
 package com.restful.service;
 
+import com.restful.dto.kecamatan.*;
+import com.restful.exception.KecamatanNotFoundException;
+
+import java.util.List;
+
 public interface KecamatanService {
 
     // create kecamatan
+    KecamatanResponseDto createKecamatan(CreateKecamatanRequestDto createKecamatanRequestDto);
 
     // get kecamatan by id
+    KecamatanResponseDto getKecamatanById(String id) throws KecamatanNotFoundException;
 
     // get all kecamatan
+    ListKecamatanResponseDto getAllKecamatan(ListKecamatanRequestDto listKecamatanRequestDto);
 
     // update kecamatan
+    KecamatanResponseDto updateKecamatan(String id, UpdateKecamatanRequestDto updateKecamatanRequestDto) throws KecamatanNotFoundException;
 
     // delete kecamatan
+    void deleteKecamatan(String id);
+
+    // get kecamatan by name
+    KecamatanResponseDto getKecamatanByName(String name) throws KecamatanNotFoundException;
 
     // get kecamatan by name contains
+    List<KecamatanResponseDto> getKecamatanByNameContains(String name);
 
     // get kecamatan by code
+    KecamatanResponseDto getKecamatanByCode(String code) throws KecamatanNotFoundException;
 
     // get kecamatan by kota id, return is List of Kecamatan
+    List<KecamatanResponseDto> getKecamatanByKotaId(String kotaId);
 }
