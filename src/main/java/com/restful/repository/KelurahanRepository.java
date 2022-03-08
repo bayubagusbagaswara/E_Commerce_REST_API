19e4fefff72bd47aaca20f8878fdaf441e767a5b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 08:48:20 +0700

    add method findByKecamatanId KelurahanRepository

--- src/main/java/com/restful/repository/KelurahanRepository.java
+++ src/main/java/com/restful/repository/KelurahanRepository.java
@@ -11,4 +11,5 @@ public interface KelurahanRepository extends JpaRepository<Kelurahan, String> {
     Optional<Kelurahan> findAllByCode(String code);
     Optional<Kelurahan> findAllByNameIgnoreCase(String name);
     List<Kelurahan> findAllByNameContainingIgnoreCase(String name);
+    List<Kelurahan> findAllByKecamatanId(String kecamatanId);
 }
