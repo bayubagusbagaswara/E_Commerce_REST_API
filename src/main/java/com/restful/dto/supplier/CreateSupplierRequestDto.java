eeaba11978d4eddad4a9d1bea9037c1eef5e53fc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 13:54:14 +0700

    update dto CreateSupplierRequestDto

--- src/main/java/com/restful/dto/supplier/CreateSupplierRequestDto.java
+++ src/main/java/com/restful/dto/supplier/CreateSupplierRequestDto.java
@@ -1,7 +1,9 @@
 package com.restful.dto.supplier;
 
+import com.restful.entity.enumerator.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,8 +22,8 @@ public class CreateSupplierRequestDto {
     @NotBlank(message = "Email must not be blank")
     private String email;
 
-    @NotBlank(message = "Gender must not be blank")
-    private String gender;
+    @NotNull(message = "Gender can not null")
+    private Gender gender;
 
     @NotBlank(message = "Mobile phone must not be blank")
     private String mobilePhone;
