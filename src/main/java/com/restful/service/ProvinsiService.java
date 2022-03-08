1d7ba08492eed02eca5bfc2c017983dc60664209
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 14:00:18 +0700

    update interface service ProvinsiService

--- src/main/java/com/restful/service/ProvinsiService.java
+++ src/main/java/com/restful/service/ProvinsiService.java
@@ -7,26 +7,19 @@ import java.util.List;
 
 public interface ProvinsiService {
 
-    // create provinsi
     ProvinsiResponseDto createProvinsi(CreateProvinsiRequestDto createProvinsiRequestDto);
 
-    // get provinsi by id
     ProvinsiResponseDto getProvinsiById(String id) throws ProvinsiNotFoundException;
 
-    // get all provinsi
     ListProvinsiResponseDto getAllProvinsi(ListProvinsiRequestDto listProvinsiRequestDto);
 
-    // update provinsi
     ProvinsiResponseDto updateProvinsi(String id, UpdateProvinsiRequestDto updateProvinsiRequestDto) throws ProvinsiNotFoundException;
 
-    // delete provinsi
     void deleteProvinsi(String id);
 
     ProvinsiResponseDto getProvinsiByName(String name) throws ProvinsiNotFoundException;
 
-    // get provinsi by name contains
     List<ProvinsiResponseDto> getProvinsiByNameContains(String name);
 
-    // get provinsi by code
     ProvinsiResponseDto getProvinsiByCode(String code) throws ProvinsiNotFoundException;
 }
