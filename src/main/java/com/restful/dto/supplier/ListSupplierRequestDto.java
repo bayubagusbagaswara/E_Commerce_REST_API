545ca2219f6834ae0aa1e8a31494e785aa34cd85
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 13:54:42 +0700

    update dto ListSupplierRequestDto

--- src/main/java/com/restful/dto/supplier/ListSupplierRequestDto.java
+++ src/main/java/com/restful/dto/supplier/ListSupplierRequestDto.java
@@ -1,4 +1,29 @@
 package com.restful.dto.supplier;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ListSupplierRequestDto {
+    @NotNull(message = "Page Index can not null")
+    @Min(value = 0, message = "Page Index must not be less than zero")
+    private Integer pageNo;
+
+    @NotNull(message = "Page Size can not null")
+    @Min(value = 1, message = "Page Size must not be less than one")
+    private Integer pageSize;
+
+    @NotBlank(message = "Sort by field must not be blank")
+    private String sortBy;
+
+    @NotBlank(message = "Sort direction must be asc or desc")
+    private String sortDir;
 }
