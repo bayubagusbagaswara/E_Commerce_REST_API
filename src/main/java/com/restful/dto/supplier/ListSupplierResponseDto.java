7bcfcd5c6c1db2d0bb953d8548e96ef556602f6b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 13:54:57 +0700

    update dto ListSupplierResponseDto

--- src/main/java/com/restful/dto/supplier/ListSupplierResponseDto.java
+++ src/main/java/com/restful/dto/supplier/ListSupplierResponseDto.java
@@ -1,4 +1,22 @@
 package com.restful.dto.supplier;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ListSupplierResponseDto {
+
+    private List<SupplierResponseDto> supplierResponseDtoList;
+    private Integer pageNo;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean last;
 }
