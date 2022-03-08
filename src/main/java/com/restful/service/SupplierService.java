9a7e96b3bc9ec0dd33e99a2a065066bb0385f83d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 07:26:04 +0700

    update method contract service SupplierService

--- src/main/java/com/restful/service/SupplierService.java
+++ src/main/java/com/restful/service/SupplierService.java
@@ -1,26 +1,41 @@
 package com.restful.service;
 
+import com.restful.dto.supplier.*;
+import com.restful.exception.ProductNotFoundException;
+import com.restful.exception.SupplierNotFoundException;
+
+import java.util.List;
+
 public interface SupplierService {
 
     // create supplier
     // user baru harus mengirimkan data:
     // name, email, mobile phone, gender, address
     // address terdiri dari street, postal code, dan kelurahan id
+    SupplierResponseDto createSupplier(CreateSupplierRequestDto createSupplierRequestDto);
 
     // get supplier by id
+    SupplierResponseDto getSupplierById(String id);
 
     // get all supplier
+    ListSupplierResponseDto getAllSuppliers(ListSupplierRequestDto listSupplierRequestDto);
 
     // update supplier
+    SupplierResponseDto updateSupplier(String id, UpdateSupplierRequestDto updateSupplierRequestDto) throws SupplierNotFoundException;
 
     // delete supplier
+    void deleteSupplier(String id);
 
     // get supplier by name
+    SupplierResponseDto getSupplierByName(String name) throws SupplierNotFoundException;
 
     // get supplier by email
+    SupplierResponseDto getSupplierByEmail(String email) throws SupplierNotFoundException;
 
     // get supplier by product
     // jadi product A memiliki supplier siapa saja
+    List<SupplierResponseDto> getSupplierByProductId(String productId);
 
     // add product to supplier
+    SupplierResponseDto addProductToSupplier(String supplierId, String productId) throws SupplierNotFoundException, ProductNotFoundException;
 }
