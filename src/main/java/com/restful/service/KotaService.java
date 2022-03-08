a8a4cd9dcecd3c41297e796ddbb3dc59ae5ef25f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 14:00:27 +0700

    update interface service KotaService

--- src/main/java/com/restful/service/KotaService.java
+++ src/main/java/com/restful/service/KotaService.java
@@ -8,30 +8,21 @@ import java.util.List;
 
 public interface KotaService {
 
-    // create kota
     KotaResponseDto createKota(CreateKotaRequestDto createKotaRequestDto) throws ProvinsiNotFoundException;
 
-    // get kota by id
     KotaResponseDto getKotaById(String id) throws KotaNotFoundException;
 
-    // get all kota
     ListKotaResponseDto getAllKota(ListKotaRequestDto listKotaRequestDto);
 
-    // update kota
     KotaResponseDto updateKota(String id, UpdateKotaRequestDto updateKotaRequestDto) throws KotaNotFoundException, ProvinsiNotFoundException;
 
-    // delete kota by id
     void deleteKota(String id);
 
-    // get Kota by name
     KotaResponseDto getKotaByName(String name) throws KotaNotFoundException;
 
-    // get kota by name contains
     List<KotaResponseDto> getKotaByNameContains(String name);
 
-    // get kota by code
     KotaResponseDto getKotaByCode(String code) throws KotaNotFoundException;
 
-    // get kota by provinsi id, balikan adalah List of Kota
     List<KotaResponseDto> getKotaByProvinsiId(String provinsiId);
 }
