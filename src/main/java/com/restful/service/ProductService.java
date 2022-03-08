8fd3fca4198636d774dd8cf5ad16c398cc9a5e54
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 14:01:22 +0700

    update interface service ProductService

--- src/main/java/com/restful/service/ProductService.java
+++ src/main/java/com/restful/service/ProductService.java
@@ -9,34 +9,23 @@ import java.util.List;
 
 public interface ProductService {
 
-    // create new product
     ProductResponseDto createProduct(CreateProductRequestDto createProductRequestDto) throws CategoryNotFoundException, ProductDetailNotFoundException;
 
-    // get product by id
     ProductResponseDto getProductById(String productId) throws ProductNotFoundException;
 
-    // list all product
     ListProductResponseDto getAllProducts(ListProductRequestDto listProductRequestDto);
 
-    // update product by id
     ProductResponseDto updateProduct(String productId, UpdateProductRequestDto updateProductRequestDto) throws ProductNotFoundException, CategoryNotFoundException;
 
-    // delete product by id
     void deleteProduct(String productId) throws ProductNotFoundException;
 
-    // get product by name fix
     ProductResponseDto getProductByName(String name) throws ProductNotFoundException;
 
-    // get product by containing name
     List<ProductResponseDto> getProductByContainingName(String name);
 
-    // get product by SKU
     ProductResponseDto getProductBySku(String sku) throws ProductNotFoundException;
 
-    // get all product by category id
     List<ProductResponseDto> getProductByCategoryId(String categoryId);
 
-    // get product by supplier
-    // jadi supplier ini memiliki product apa saja
     List<ProductResponseDto> getProductBySuppliersId(String supplierId);
 }
