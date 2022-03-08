5df1cc565b5d0822251ff7698d0ce6e85c1093fb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-08 14:00:35 +0700

    update interface service KecamatanService

--- src/main/java/com/restful/service/KecamatanService.java
+++ src/main/java/com/restful/service/KecamatanService.java
@@ -8,30 +8,21 @@ import java.util.List;
 
 public interface KecamatanService {
 
-    // create kecamatan
     KecamatanResponseDto createKecamatan(CreateKecamatanRequestDto createKecamatanRequestDto) throws KotaNotFoundException;
 
-    // get kecamatan by id
     KecamatanResponseDto getKecamatanById(String id) throws KecamatanNotFoundException;
 
-    // get all kecamatan
     ListKecamatanResponseDto getAllKecamatan(ListKecamatanRequestDto listKecamatanRequestDto);
 
-    // update kecamatan
     KecamatanResponseDto updateKecamatan(String id, UpdateKecamatanRequestDto updateKecamatanRequestDto) throws KecamatanNotFoundException, KotaNotFoundException;
 
-    // delete kecamatan
     void deleteKecamatan(String id);
 
-    // get kecamatan by name
     KecamatanResponseDto getKecamatanByName(String name) throws KecamatanNotFoundException;
 
-    // get kecamatan by name contains
     List<KecamatanResponseDto> getKecamatanByNameContains(String name);
 
-    // get kecamatan by code
     KecamatanResponseDto getKecamatanByCode(String code) throws KecamatanNotFoundException;
 
-    // get kecamatan by kota id, return is List of Kecamatan
     List<KecamatanResponseDto> getKecamatanByKotaId(String kotaId);
 }
