42fcfc3d09993937c3dce78be85687e715a352aa
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-10 20:55:14 +0700

    update dto UpdateProductRequestDto

--- src/main/java/com/restful/dto/product/UpdateProductRequestDto.java
+++ src/main/java/com/restful/dto/product/UpdateProductRequestDto.java
@@ -1,5 +1,8 @@
 package com.restful.dto.product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +16,25 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class UpdateProductRequestDto {
 
+    @NotBlank(message = "Product name must not be blank")
     private String name;
+
+    @NotBlank(message = "Product SKU must not be blank")
+    @Size(max = 50, message = "Product SKU maximum length is 50 characters")
+    private String sku;
+
+    @NotNull(message = "Product price can not null")
+    @Size(min = 1, message = "Product price must be minimum 1")
     private BigDecimal price;
+
+    @NotNull(message = "Product quantity can not null")
+    @Size(min = 0, message = "Product quantity must be minimum 0")
     private Integer quantity;
-    private String productDetailId;
+
+    @NotBlank(message = "Product description must not be blank")
+    @Size(max = 500, message = "Product description maximum length is 500 characters")
+    private String description;
+
+    @NotBlank(message = "Category ID must not be blank")
     private String categoryId;
 }
