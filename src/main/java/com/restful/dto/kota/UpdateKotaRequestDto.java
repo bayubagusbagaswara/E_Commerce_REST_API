61cca0787761610419edb243dd954ebfaa736b0d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 20:10:00 +0700

    update dto UpdateKotaRequestDto

--- src/main/java/com/restful/dto/kota/UpdateKotaRequestDto.java
+++ src/main/java/com/restful/dto/kota/UpdateKotaRequestDto.java
@@ -1,22 +1,18 @@
 package com.restful.dto.kota;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class UpdateKotaRequestDto {
 
-    @NotBlank(message = "Kota name must not be blank")
-    private String name;
-
     @NotBlank(message = "Kota code must not be blank")
     private String code;
+
+    @NotBlank(message = "Kota name must not be blank")
+    private String name;
 
     @NotBlank(message = "Provinsi ID must not be blank")
     private String provinsiId;
