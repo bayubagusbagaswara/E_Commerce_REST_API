ae6ef221d651f6fefdbbb04ace7e0f350b9a65a8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 20:24:32 +0700

    update dto ListKelurahanRequestDto

--- src/main/java/com/restful/dto/kelurahan/ListKelurahanRequestDto.java
+++ src/main/java/com/restful/dto/kelurahan/ListKelurahanRequestDto.java
@@ -3,26 +3,24 @@ package com.restful.dto.kelurahan;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ListKelurahanRequestDto {
 
-    @NotNull(message = "Page Index can not null")
-    @Min(value = 0, message = "Page Index must not be less than zero")
+    @NotNull(message = "Page index can not null")
+    @Min(value = 0, message = "Page index must not be less than zero")
     private Integer pageNo;
 
-    @NotNull(message = "Page Size can not null")
-    @Min(value = 1, message = "Page Size must not be less than one")
+    @NotNull(message = "Page size can not null")
+    @Min(value = 1, message = "Page size must not be less than one")
     private Integer pageSize;
 
-    @NotBlank(message = "Property must not be blank")
+    @NotBlank(message = "Sort by certain data required")
     private String sortBy;
 
-    @NotBlank(message = "Sort direction must be asc or desc")
+    @NotBlank(message = "Sort direction must be asc or desc and required")
     private String sortDir;
 }
