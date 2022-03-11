3535701f1b305b55285cf561d3f4c6e4832f5f77
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 16:10:49 +0700

    add sample-data-products-suppliers.sql

---
+++ src/test/resources/sql/sample-data-products-suppliers.sql
@@ -0,0 +1,17 @@
+
+-- bayu [sony-alpha, macbook-pro-2021]
+-- bagus [ipad, apple-magic]
+-- albert [iphone, macbook-pro-2021]
+-- newton [iphone, apple-magic]
+-- tesla [macbook-pro-2021]
+
+insert into product_supplier (product_id, supplier_id)
+values ('sony-alpha', 'bayu'),
+        ('apple-magic', 'bagus'),
+        ('ipad', 'bagus')
+        ('iphone', 'albert'),
+        ('iphone', 'newton'),
+        ('apple-magic', 'newton'),
+        ('macbook-pro-2021', 'bayu'),
+        ('macbook-pro-2021', 'albert'),
+        ('macbook-pro-2021', 'tesla');
