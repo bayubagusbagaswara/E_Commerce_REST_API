de4f74ea88beb919f05510c5be295d4d65f977d6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 18:52:06 +0700

    update dto ProvinsiResponseDto

--- src/main/java/com/restful/dto/provinsi/ProvinsiResponseDto.java
+++ src/main/java/com/restful/dto/provinsi/ProvinsiResponseDto.java
@@ -1,19 +1,17 @@
 package com.restful.dto.provinsi;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ProvinsiResponseDto {
 
     private String id;
-    private String name;
     private String code;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private String name;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
