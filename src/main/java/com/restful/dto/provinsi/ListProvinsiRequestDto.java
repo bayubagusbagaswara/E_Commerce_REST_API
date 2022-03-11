99bbb44d8c5542599c8b22f4179c02a7a4233294
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 18:55:02 +0700

    update dto ListProvinsiRequestDto

--- src/main/java/com/restful/dto/provinsi/ListProvinsiRequestDto.java
+++ src/main/java/com/restful/dto/provinsi/ListProvinsiRequestDto.java
@@ -3,20 +3,18 @@ package com.restful.dto.provinsi;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ListProvinsiRequestDto {
 
-    @NotNull(message = "Page Index can not null")
+    @NotNull(message = "Page index can not null")
     @Min(value = 0, message = "Page Index must not be less than zero")
     private Integer pageNo;
 
-    @NotNull(message = "Page Size can not null")
+    @NotNull(message = "Page size can not null")
     @Min(value = 1, message = "Page Size must not be less than one")
     private Integer pageSize;
 
