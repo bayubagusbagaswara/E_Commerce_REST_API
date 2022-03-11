f9f4f0eb763a2c5a9ff719c6475ae414bc7c8a13
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 20:09:50 +0700

    update dto ListKotaResponseDto

--- src/main/java/com/restful/dto/kota/ListKotaResponseDto.java
+++ src/main/java/com/restful/dto/kota/ListKotaResponseDto.java
@@ -1,17 +1,15 @@
 package com.restful.dto.kota;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ListKotaResponseDto {
 
-    private List<KotaResponseDto> kotaResponses;
+    private List<KotaResponseDto> kotaResponseList;
     private Integer pageNo;
     private Integer pageSize;
     private Long totalElements;
