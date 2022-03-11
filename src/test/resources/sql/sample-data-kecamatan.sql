b90cf3cbce0224243299f2541192f16badc1ab51
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 16:15:30 +0700

    update sample-data-kecamatan.sql

--- src/test/resources/sql/sample-data-kecamatan.sql
+++ src/test/resources/sql/sample-data-kecamatan.sql
@@ -1,35 +1,35 @@
 -- KECAMATAN DI KOTA JAKARTA PUSAT, PROVINSI JAKARTA
-insert into kecamatan (id, name, code, created_by, created_at, status_record, id_kota)
-values ('317103', 'Kemayoran', '317103', 'Current User', current_timestamp, 'ACTIVE', '3171'),
-        ('317106', 'Menteng', '317106', 'Current User', current_timestamp, 'ACTIVE', '3171');
+insert into kecamatan (id, name, code, created_by, created_date, status_record, id_kota)
+values ('317103', 'Kemayoran', '317103', 'User', current_timestamp, 'ACTIVE', '3171'),
+        ('317106', 'Menteng', '317106', 'User', current_timestamp, 'ACTIVE', '3171');
 
 -- KECAMATAN DI JAKARTA SELATAN, PROVINSI JAKARTA
-insert into kecamatan (id, name, code, created_by, created_at, status_record, id_kota)
-values ('317401', 'Tebet', '317401', 'Current User', current_timestamp, 'ACTIVE', '3174'),
-        ('317405', 'Kebayoran Lama', '317405', 'Current User', current_timestamp, 'ACTIVE', '3174'),
-        ('317407', 'Kebayoran Baru', '317407', 'Current User', current_timestamp, 'ACTIVE', '3174');
+insert into kecamatan (id, name, code, created_by, created_date, status_record, id_kota)
+values ('317401', 'Tebet', '317401', 'User', current_timestamp, 'ACTIVE', '3174'),
+        ('317405', 'Kebayoran Lama', '317405', 'User', current_timestamp, 'ACTIVE', '3174'),
+        ('317407', 'Kebayoran Baru', '317407', 'User', current_timestamp, 'ACTIVE', '3174');
 
 -- KECAMATAN DI KOTA KEDIRI, PROVINSI JAWA TIMUR
-insert into kecamatan (id, name, code, created_by, created_at, status_record, id_kota)
-values ('357101', 'Mojoroto', '357101', 'Current User', current_timestamp, 'ACTIVE', '3571'),
-        ('357102', 'Kota', '357102', 'Current User', current_timestamp, 'ACTIVE', '3571'),
-        ('357103', 'Pesantren', '357103', 'Current User', current_timestamp, 'ACTIVE', '3571');
+insert into kecamatan (id, name, code, created_by, created_date, status_record, id_kota)
+values ('357101', 'Mojoroto', '357101', 'User', current_timestamp, 'ACTIVE', '3571'),
+        ('357102', 'Kota', '357102', 'User', current_timestamp, 'ACTIVE', '3571'),
+        ('357103', 'Pesantren', '357103', 'User', current_timestamp, 'ACTIVE', '3571');
 
 -- KECAMATAN DI KOTA SURABAYA, PROVINSI JAWA TIMUR
-insert into kecamatan (id, name, code, created_by, created_at, status_record, id_kota)
-values ('357809', 'Sukolilo', '357809', 'Current User', current_timestamp, 'ACTIVE', '3578'),
-        ('357826', 'Mulyorejo', '357826', 'Current User', current_timestamp, 'ACTIVE', '3578'),
-        ('357811', 'Simokerto', '357811', 'Current User', current_timestamp, 'ACTIVE', '3578'),
-        ('357817', 'Kenjeran', '357817', 'Current User', current_timestamp, 'ACTIVE', '3578'),
-        ('357831', 'Sambikerep', '357831', 'Current User', current_timestamp, 'ACTIVE', '3578');
+insert into kecamatan (id, name, code, created_by, created_date, status_record, id_kota)
+values ('357809', 'Sukolilo', '357809', 'User', current_timestamp, 'ACTIVE', '3578'),
+        ('357826', 'Mulyorejo', '357826', 'User', current_timestamp, 'ACTIVE', '3578'),
+        ('357811', 'Simokerto', '357811', 'User', current_timestamp, 'ACTIVE', '3578'),
+        ('357817', 'Kenjeran', '357817', 'User', current_timestamp, 'ACTIVE', '3578'),
+        ('357831', 'Sambikerep', '357831', 'User', current_timestamp, 'ACTIVE', '3578');
 
 -- KECAMATAN DI KAB BADUNG, PROVINSI BALI
-insert into kecamatan (id, name, code, created_by, created_at, status_record, id_kota)
-values ('510301', 'Kuta', '510301', 'Current User', current_timestamp, 'ACTIVE', '5103');
+insert into kecamatan (id, name, code, created_by, created_date, status_record, id_kota)
+values ('510301', 'Kuta', '510301', 'User', current_timestamp, 'ACTIVE', '5103');
 
 -- KECAMATAN DI KOTA DENPASAR, PROVINSI BALI
-insert into kecamatan (id, name, code, created_by, created_at, status_record, id_kota)
-values ('517101', 'Denpasar Selatan', '517101', 'Current User', current_timestamp, 'ACTIVE', '5171'),
-        ('517102', 'Denpasar Timur', '517102', 'Current User', current_timestamp,'ACTIVE', '5171'),
-        ('517103', 'Denpasar Barat', '517103', 'Current User', current_timestamp, 'ACTIVE', '5171'),
-        ('517104', 'Denpasar Utara', '517104', 'Current User', current_timestamp, 'ACTIVE', '5171');
+insert into kecamatan (id, name, code, created_by, created_date, status_record, id_kota)
+values ('517101', 'Denpasar Selatan', '517101', 'User', current_timestamp, 'ACTIVE', '5171'),
+        ('517102', 'Denpasar Timur', '517102', 'User', current_timestamp, 'ACTIVE', '5171'),
+        ('517103', 'Denpasar Barat', '517103', 'User', current_timestamp, 'ACTIVE', '5171'),
+        ('517104', 'Denpasar Utara', '517104', 'User', current_timestamp, 'ACTIVE', '5171');
