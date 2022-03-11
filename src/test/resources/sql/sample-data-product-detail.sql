c875d5fb911d3da223971d32e68de88e4d7af392
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 16:04:09 +0700

    add sample-data-product-detail.sql

---
+++ src/test/resources/sql/sample-data-product-detail.sql
@@ -0,0 +1,32 @@
+-- TOTAL SAMPLE DATA = 11
+-- ELECTRONIC - TELEVISION
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-redmibook-15', 'SKU-RedmiBook15', 'RedmiBook 15 description', 'User', current_timestamp, 'ACTIVE');
+
+-- ELECTRONIC - CAMERA
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-canon-eos', 'SKU-CanonEOS', 'Canon EOS600D description', 'User', current_timestamp, 'ACTIVE');
+
+-- COMPUTER AND ACCESSORIES - LAPTOP
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-macbook-pro-2021', 'SKU-AppleMacBookPro2021', 'Apple MacBook Pro 2021 description', 'User', current_timestamp, 'ACTIVE'),
+        ('pd-lenovo-legion-5', 'SKU-Legion5', 'Lenovo Legion 5 description', 'User', current_timestamp, 'ACTIVE');
+
+-- COMPUTER AND ACCESSORIES - MONITOR
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-lg-24mk430h', 'SKU-LG24MK430HB', 'LG 24MK430 description', 'User', current_timestamp, 'ACTIVE');
+
+-- COMPUTER AND ACCESSORIES - MOUSE
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-apple-magic', 'SKU-AppleMagic', 'Apple Magic description', 'User', current_timestamp, 'ACTIVE');
+
+-- HAND PHONE AND ACCESSORIES - PHONE
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-samsung-z', 'SKU-SamsungZ', 'Samsung Z description', 'User', current_timestamp, 'ACTIVE'),
+        ('pd-iphone', 'SKU-iPhone13', 'Apple iPhone 13 description', 'User', current_timestamp, 'ACTIVE'),
+        ('pd-redmi-9', 'SKU-Redmi9', 'Xiaomi Redmi 9 description', 'User', current_timestamp, 'ACTIVE');
+
+-- HAND PHONE AND ACCESSORIES - TABLET
+insert into product_detail(id, sku, description, created_by, created_date, status_record)
+values ('pd-tab-s6', 'SKU-SamsungTabS6', 'Samsung Tab S6 description', 'User', current_timestamp, 'ACTIVE'),
+        ('pd-ipad', 'SKU-iPadPro', 'Apple iPad description', 'User', current_timestamp, 'ACTIVE');
