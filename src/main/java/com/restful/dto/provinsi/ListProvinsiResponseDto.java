015b2e668f62b09d138de6a3671a21eb4aae609f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 18:55:18 +0700

    update dto ListProvinsiResponseDto

--- src/main/java/com/restful/dto/provinsi/ListProvinsiResponseDto.java
+++ src/main/java/com/restful/dto/provinsi/ListProvinsiResponseDto.java
@@ -1,19 +1,15 @@
 package com.restful.dto.provinsi;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class ListProvinsiResponseDto {
 
-    private List<ProvinsiResponseDto> provinsiResponses;
+    private List<ProvinsiResponseDto> provinsiResponseList;
     private Integer pageNo;
     private Integer pageSize;
     private Long totalElements;
