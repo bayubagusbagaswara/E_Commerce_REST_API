77b97ad2896a337298c66c52b54bebcb5cd8bccf
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 18:55:37 +0700

    update dto UpdateProvinsiRequestDto

--- src/main/java/com/restful/dto/provinsi/UpdateProvinsiRequestDto.java
+++ src/main/java/com/restful/dto/provinsi/UpdateProvinsiRequestDto.java
@@ -1,18 +1,16 @@
 package com.restful.dto.provinsi;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class UpdateProvinsiRequestDto {
 
-    @NotBlank(message = "Provinsi name must not be blank")
-    private String name;
-
     @NotBlank(message = "Provinsi code must not be blank")
     private String code;
+
+    @NotBlank(message = "Provinsi name must not be blank")
+    private String name;
 }
