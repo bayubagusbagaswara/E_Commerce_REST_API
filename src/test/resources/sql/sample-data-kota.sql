d6295514f765da46b50e0d3de5c6fb3700b15255
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 16:15:22 +0700

    update sample-data-kota.sql

--- src/test/resources/sql/sample-data-kota.sql
+++ src/test/resources/sql/sample-data-kota.sql
@@ -1,15 +1,15 @@
 -- KOTA DI PROVINSI JAKARTA
-insert into kota (id, name, code, created_by, created_at, status_record, id_provinsi)
-values ('3171', 'KOTA ADM. JAKARTA PUSAT', '3171', 'Current User', current_timestamp, 'ACTIVE', '31'),
-        ('3174', 'KOTA ADM. JAKARTA SELATAN', '3174', 'Current User', current_timestamp, 'ACTIVE', '31');
+insert into kota (id, name, code, created_by, created_date, status_record, id_provinsi)
+values ('3171', 'KOTA ADM. JAKARTA PUSAT', '3171', 'User', current_timestamp, 'ACTIVE', '31'),
+        ('3174', 'KOTA ADM. JAKARTA SELATAN', '3174', 'User', current_timestamp, 'ACTIVE', '31');
 
 -- KOTA DI PROVINSI JAWA TIMUR
-insert into kota (id, name, code, created_by, created_at, status_record, id_provinsi)
-values ('3571', 'KOTA KEDIRI', '3571', 'Current User', current_timestamp, 'ACTIVE', '35'),
-        ('3506', 'KAB. KEDIRI', '3506', 'Current User', current_timestamp, 'ACTIVE', '35'),
-        ('3578', 'KOTA SURABAYA', '3578', 'Current User', current_timestamp, 'ACTIVE', '35');
+insert into kota (id, name, code, created_by, created_date, status_record, id_provinsi)
+values ('3571', 'KOTA KEDIRI', '3571', 'User', current_timestamp, 'ACTIVE', '35'),
+        ('3506', 'KAB. KEDIRI', '3506', 'User', current_timestamp, 'ACTIVE', '35'),
+        ('3578', 'KOTA SURABAYA', '3578', 'User', current_timestamp, 'ACTIVE', '35');
 
 -- KOTA DI PROVINSI BALI
-INSERT INTO kota ( id, name, code, created_by, created_at, status_record, id_provinsi)
-VALUES ('5103', 'KAB. BADUNG', '5103', 'Current User', current_timestamp, 'ACTIVE', '51'),
-        ('5171', 'KOTA DENPASAR', '5171', 'Current User', current_timestamp, 'ACTIVE', '51');
+INSERT INTO kota ( id, name, code, created_by, created_date, status_record, id_provinsi)
+VALUES ('5103', 'KAB. BADUNG', '5103', 'User', current_timestamp, 'ACTIVE', '51'),
+        ('5171', 'KOTA DENPASAR', '5171', 'User', current_timestamp, 'ACTIVE', '51');
