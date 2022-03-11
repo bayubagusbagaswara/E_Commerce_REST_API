f2b1f917b9ab0673e4085705bf709aad5282d78b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 20:24:43 +0700

    update dto ListKelurahanResponseDto

--- src/main/java/com/restful/dto/kelurahan/ListKelurahanResponseDto.java
+++ src/main/java/com/restful/dto/kelurahan/ListKelurahanResponseDto.java
@@ -1,17 +1,15 @@
 package com.restful.dto.kelurahan;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ListKelurahanResponseDto {
 
-    private List<KelurahanResponseDto> kelurahanResponses;
+    private List<KelurahanResponseDto> kelurahanResponseList;
     private Integer pageNo;
     private Integer pageSize;
     private Long totalElements;
