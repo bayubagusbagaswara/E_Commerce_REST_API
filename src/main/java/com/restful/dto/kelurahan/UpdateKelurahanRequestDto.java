a43319acbf4462f215361d0710e61675168aa403
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 20:24:56 +0700

    update dto UpdateKelurahanRequestDto

--- src/main/java/com/restful/dto/kelurahan/UpdateKelurahanRequestDto.java
+++ src/main/java/com/restful/dto/kelurahan/UpdateKelurahanRequestDto.java
@@ -1,22 +1,18 @@
 package com.restful.dto.kelurahan;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class UpdateKelurahanRequestDto {
 
-    @NotBlank(message = "Name must not be blank")
-    private String name;
-
     @NotBlank(message = "Code must not be blank")
     private String code;
+
+    @NotBlank(message = "Name must not be blank")
+    private String name;
 
     @NotBlank(message = "Kecamatan ID must not be blank")
     private String kecamatanId;
