069595ac90049f9e7a03d3c2b1412f59ef8a283d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-11 20:09:11 +0700

    update dto ListKotaRequestDto

--- src/main/java/com/restful/dto/kota/ListKotaRequestDto.java
+++ src/main/java/com/restful/dto/kota/ListKotaRequestDto.java
@@ -3,26 +3,22 @@ package com.restful.dto.kota;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class ListKotaRequestDto {
 
-    @NotNull(message = "Page Index can not null")
-    @Min(value = 0, message = "Page Index must not be less than zero")
+    @NotNull(message = "Page index can not null")
+    @Min(value = 0, message = "Page index must not be less than zero")
     private Integer pageNo;
 
-    @NotNull(message = "Page Size can not null")
-    @Min(value = 1, message = "Page Size must not be less than one")
+    @NotNull(message = "Page size can not null")
+    @Min(value = 1, message = "Page size must not be less than one")
     private Integer pageSize;
 
-    @NotBlank(message = "Property must not be blank")
+    @NotBlank(message = "Sort by must not be blank")
     private String sortBy;
 
     @NotBlank(message = "Sort direction must be asc or desc")
