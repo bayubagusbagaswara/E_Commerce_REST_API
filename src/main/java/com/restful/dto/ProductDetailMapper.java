eb733a1070870a43dc631ab7b9d214ebb059be80
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-12 08:35:53 +0700

    update ProductDetailMapper

--- src/main/java/com/restful/dto/ProductDetailMapper.java
+++ src/main/java/com/restful/dto/ProductDetailMapper.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProductDetailMapper {
 
-    public ProductDetailResponseDto mapProductDetailToProductDetailResponseDto(ProductDetail productDetail) {
-        ProductDetailResponseDto productDetailDto = new ProductDetailResponseDto();
-        productDetailDto.setId(productDetail.getId());
-        productDetailDto.setSku(productDetail.getSku());
-        productDetailDto.setDescription(productDetail.getDescription());
-        productDetailDto.setCreatedAt(productDetail.getCreatedAt());
-        productDetailDto.setUpdatedAt(productDetail.getUpdatedAt());
-        return productDetailDto;
+    public ProductDetailResponseDto mapToProductDetailResponse(ProductDetail productDetail) {
+        ProductDetailResponseDto productDetailResponse = new ProductDetailResponseDto();
+        productDetailResponse.setId(productDetail.getId());
+        productDetailResponse.setSku(productDetail.getSku());
+        productDetailResponse.setDescription(productDetail.getDescription());
+        productDetailResponse.setCreatedAt(productDetail.getCreatedDate();
+        productDetailResponse.setUpdatedAt(productDetail.getUpdatedDate());
+        return productDetailResponse;
     }
 }
