983d09da13842a3755e10ab6f2eec0bad62ad082
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-12 12:28:51 +0700

    update sample-data-address.sql

--- src/test/resources/sql/sample-data-address.sql
+++ src/test/resources/sql/sample-data-address.sql
@@ -1,32 +1,32 @@
 -- TOTAL SAMPLE DATA = 8
 -- address Singonegaran, Kediri
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('singonegaran-kdr', 'Jalan Dewi Sartika', '64132', '3571031005', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('singonegaran-kdr', 'Jalan Dewi Sartika', '64132', '3571031005', current_timestamp, 'User', 'ACTIVE');
 
 -- address Pesantren, Kediri
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('pesantren-kdr', 'Jalan Pesantren', '64131', '3571031013', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('pesantren-kdr', 'Jalan Pesantren', '64131', '3571031013', current_timestamp, 'User', 'ACTIVE');
 
 -- address Ngronggo, Kediri
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('ngronggo-kdr', 'Jalan Perintis Kemerdekaan', '64127', '3571021014', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('ngronggo-kdr', 'Jalan Perintis Kemerdekaan', '64127', '3571021014', current_timestamp, 'User', 'ACTIVE');
 
 -- address Gebang Putih, Surabaya
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('gebang-putih-sby', 'Jalan Gebang Putih', '60117', '3578091002', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('gebang-putih-sby', 'Jalan Gebang Putih', '60117', '3578091002', current_timestamp, 'User', 'ACTIVE');
 
 -- address Keputih, Surabaya
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('keputih-sby', 'Jalan Keputih', '60111', '3578091001', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('keputih-sby', 'Jalan Keputih', '60111', '3578091001', current_timestamp, 'User', 'ACTIVE');
 
 -- address Manggarai, Jakarta Selatan
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('manggarai-jaksel', 'Jalan Dr Saharjo', '12850', '3174011007', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('manggarai-jaksel', 'Jalan Dr Saharjo', '12850', '3174011007', current_timestamp, 'User', 'ACTIVE');
 
 -- address Tebet Timur, Jakarta Selatan
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('tebet-jaksel', 'Jalan Tebet Timur Raya', '12820', '3174011001', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('tebet-jaksel', 'Jalan Tebet Timur Raya', '12820', '3174011001', current_timestamp, 'User', 'ACTIVE');
 
 -- address Gandaria Utara, Jakarta Selatan
-insert into address(id, street, postal_code, id_kelurahan, created_at, created_by)
-values ('gandaria-jaksel', 'Jalan Mahali', '12140', '3174071009', current_timestamp, 'User');
+insert into address(id, street, postal_code, id_kelurahan, created_date, created_by, status_record)
+values ('gandaria-jaksel', 'Jalan Mahali', '12140', '3174071009', current_timestamp, 'User', 'ACTIVE');
