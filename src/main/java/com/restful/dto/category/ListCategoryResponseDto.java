bfcdc68b755cf4ff5a754d9fabdaf2662fd553e8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-12 08:09:09 +0700

    update dto ListCategoryResponseDto

--- src/main/java/com/restful/dto/category/ListCategoryResponseDto.java
+++ src/main/java/com/restful/dto/category/ListCategoryResponseDto.java
@@ -1,28 +1,18 @@
 package com.restful.dto.category;
 
-import com.restful.dto.product.ProductResponseDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
-public class ListAllCategoryResponseDto {
+public class ListCategoryResponseDto {
 
-    private List<CategoryResponseDto> categoryResponseDtoList;
-
+    private List<CategoryResponseDto> categoryResponseList;
     private Integer pageNo;
-
     private Integer pageSize;
-
     private Long totalElements;
-
     private Integer totalPages;
-
     private boolean last;
 }
