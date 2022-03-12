910c382d747f53a101fc8624d57909ae3dcf1703
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-12 08:06:48 +0700

    update dto CreateProductRequestDto

--- src/main/java/com/restful/dto/product/CreateProductRequestDto.java
+++ src/main/java/com/restful/dto/product/CreateProductRequestDto.java
@@ -3,14 +3,12 @@ package com.restful.dto.product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class CreateProductRequestDto {
 
