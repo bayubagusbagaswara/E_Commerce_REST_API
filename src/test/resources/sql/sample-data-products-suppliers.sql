e39b8a12a797d4b7812834a14a7ab240f49d4609
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-13 15:22:02 +0700

    update schema sample-data-products-suppliers.sql

--- src/test/resources/sql/sample-data-products-suppliers.sql
+++ src/test/resources/sql/sample-data-products-suppliers.sql
@@ -1,17 +1,14 @@
-
--- bayu [sony-alpha, macbook-pro-2021]
--- bagus [ipad, apple-magic]
--- albert [iphone, macbook-pro-2021]
--- newton [iphone, apple-magic]
--- tesla [macbook-pro-2021]
-
-insert into product_supplier (product_id, supplier_id)
-values ('sony-alpha', 'bayu'),
-        ('apple-magic', 'bagus'),
-        ('ipad', 'bagus')
+-- yang dihapus adalah supplier albert dan product macbook
+insert into product_supplier (id_product, id_supplier)
+values ('redmibook', 'bayu'),
+        ('legion', 'tesla'),
+        ('ipad', 'bagus'),
+        ('ipad', 'bayu'),
         ('iphone', 'albert'),
         ('iphone', 'newton'),
-        ('apple-magic', 'newton'),
-        ('macbook-pro-2021', 'bayu'),
-        ('macbook-pro-2021', 'albert'),
-        ('macbook-pro-2021', 'tesla');
+        ('canon', 'bagus'),
+        ('canon', 'newton'),
+        ('canon', 'albert'),
+        ('macbook', 'bayu'),
+        ('macbook', 'albert'),
+        ('macbook', 'tesla');
