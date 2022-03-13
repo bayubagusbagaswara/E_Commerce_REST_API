dcad877d2dfac767406a1a45cd4d7bff523ef203
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-13 15:21:06 +0700

    update schema sample-data-product-detail.sql

--- src/test/resources/sql/sample-data-product-detail.sql
+++ src/test/resources/sql/sample-data-product-detail.sql
@@ -1,20 +1,20 @@
 -- TOTAL SAMPLE DATA = 11
 -- ELECTRONIC - TELEVISION
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
-values ('pd-redmibook-15', 'SKU-RedmiBook15', 'RedmiBook 15 description', 'User', current_timestamp, 'ACTIVE');
+values ('pd-redmibook', 'SKU-RedmiBook15', 'RedmiBook 15 description', 'User', current_timestamp, 'ACTIVE');
 
 -- ELECTRONIC - CAMERA
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
-values ('pd-canon-eos', 'SKU-CanonEOS', 'Canon EOS600D description', 'User', current_timestamp, 'ACTIVE');
+values ('pd-canon', 'SKU-CanonEOS', 'Canon EOS600D description', 'User', current_timestamp, 'ACTIVE');
 
 -- COMPUTER AND ACCESSORIES - LAPTOP
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
-values ('pd-macbook-pro-2021', 'SKU-AppleMacBookPro2021', 'Apple MacBook Pro 2021 description', 'User', current_timestamp, 'ACTIVE'),
-        ('pd-lenovo-legion-5', 'SKU-Legion5', 'Lenovo Legion 5 description', 'User', current_timestamp, 'ACTIVE');
+values ('pd-macbook', 'SKU-AppleMacBook', 'Apple MacBook Pro 2021 description', 'User', current_timestamp, 'ACTIVE'),
+        ('pd-legion', 'SKU-Legion5', 'Lenovo Legion 5 description', 'User', current_timestamp, 'ACTIVE');
 
 -- COMPUTER AND ACCESSORIES - MONITOR
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
-values ('pd-lg-24mk430h', 'SKU-LG24MK430HB', 'LG 24MK430 description', 'User', current_timestamp, 'ACTIVE');
+values ('pd-lg24', 'SKU-LG24', 'LG 24MK430 description', 'User', current_timestamp, 'ACTIVE');
 
 -- COMPUTER AND ACCESSORIES - MOUSE
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
@@ -22,9 +22,9 @@ values ('pd-apple-magic', 'SKU-AppleMagic', 'Apple Magic description', 'User', c
 
 -- HAND PHONE AND ACCESSORIES - PHONE
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
-values ('pd-samsung-z', 'SKU-SamsungZ', 'Samsung Z description', 'User', current_timestamp, 'ACTIVE'),
+values ('pd-samsungz', 'SKU-SamsungZ', 'Samsung Z description', 'User', current_timestamp, 'ACTIVE'),
         ('pd-iphone', 'SKU-iPhone13', 'Apple iPhone 13 description', 'User', current_timestamp, 'ACTIVE'),
-        ('pd-redmi-9', 'SKU-Redmi9', 'Xiaomi Redmi 9 description', 'User', current_timestamp, 'ACTIVE');
+        ('pd-redmi9', 'SKU-Redmi9', 'Xiaomi Redmi 9 description', 'User', current_timestamp, 'ACTIVE');
 
 -- HAND PHONE AND ACCESSORIES - TABLET
 insert into product_detail(id, sku, description, created_by, created_date, status_record)
