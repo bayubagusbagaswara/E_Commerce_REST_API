80c848e0b72e8b67226311006150a6d10fffa428
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-14 17:02:28 +0700

    update schema migrate Product

--- src/main/resources/db/migration/V20220206__Schema_Product.sql
+++ src/main/resources/db/migration/V20220206__Schema_Product.sql
@@ -6,13 +6,12 @@ create table products (
     updated_date timestamp without time zone,
     updated_by character varying(255),
     name character varying(100) not null,
-    price numeric(19,2) not null,
-    quantity integer not null,
+    price numeric(19,2) not null default 0,
+    quantity integer not null default 0,
     id_category character varying(64),
-    id_product_detail character varying(64)
+    id_product_detail character varying(64),
+    constraint ck_price_default check (price >= 1),
+    constraint ck_quantity_default check (quantity >= 0),
+    constraint fk_product_category foreign key (id_category) references categories(id),
+    constraint fk_product_product_detail foreign key (id_product_detail) references product_detail(id)
 );
-
-alter table products
-    add constraint fk_product_category foreign key (id_category) references categories(id);
-alter table products
-    add constraint fk_product_product_detail foreign key (id_product_detail) references product_detail(id);
