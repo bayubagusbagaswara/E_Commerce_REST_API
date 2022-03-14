bf96f8d139b799dc29dd816c68898b61f008dbf2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-14 17:01:32 +0700

    update schema migrate Wilayah

--- src/main/resources/db/migration/V20220202__Schema_Wilayah.sql
+++ src/main/resources/db/migration/V20220202__Schema_Wilayah.sql
@@ -7,11 +7,9 @@ create table provinsi (
     updated_date timestamp without time zone,
     updated_by character varying(255),
     code character varying(255) not null,
-    name character varying(255) not null
+    name character varying(255) not null,
+    constraint provinsi_unique_code unique (code)
 );
-
-alter table provinsi
-    add constraint provinsi_unique_code unique (code);
 
 -- KOTA
 create table kota (
@@ -23,13 +21,10 @@ create table kota (
     updated_by character varying(255),
     code character varying(255) not null,
     name character varying(255) not null,
-    id_provinsi character varying(64)
+    id_provinsi character varying(64),
+    constraint kota_unique_code unique (code),
+    constraint fk_kota_provinsi foreign key (id_provinsi) references provinsi(id) on update cascade on delete cascade
 );
-
-alter table kota
-    add constraint kota_unique_code unique (code);
-alter table kota
-    add constraint fk_kota_provinsi foreign key (id_provinsi) references provinsi(id);
 
 -- KECAMATAN
 create table kecamatan (
@@ -41,13 +36,10 @@ create table kecamatan (
     updated_by character varying(255),
     code character varying(255) not null,
     name character varying(255) not null,
-    id_kota character varying(64)
+    id_kota character varying(64),
+    constraint kecamatan_unique_code unique (code),
+    constraint fk_kecamatan_kota foreign key (id_kota) references kota(id) on update cascade on delete cascade
 );
-
-alter table kecamatan
-    add constraint kecamatan_unique_code unique (code);
-alter table kecamatan
-    add constraint fk_kecamatan_kota foreign key (id_kota) references kota(id);
 
 -- KELURAHAN
 create table kelurahan (
@@ -59,10 +51,7 @@ create table kelurahan (
     updated_by character varying(255),
     code character varying(255) not null,
     name character varying(255) not null,
-    id_kecamatan character varying(64)
+    id_kecamatan character varying(64),
+    constraint kelurahan_unique_code unique (code),
+    constraint fk_kelurahan_kecamatan foreign key (id_kecamatan) references kecamatan(id) on update cascade on delete cascade
 );
-
-alter table kelurahan
-    add constraint kelurahan_unique_code unique (code);
-alter table kelurahan
-    add constraint fk_kelurahan_kecamatan foreign key (id_kecamatan) references kecamatan(id);
