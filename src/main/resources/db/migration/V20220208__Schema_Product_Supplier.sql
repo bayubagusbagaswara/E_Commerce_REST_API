300fbdeb7a82752115ce8a0ab9d685a7914944c0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-14 17:03:22 +0700

    update schema migrate Product Supplier

--- src/main/resources/db/migration/V20220208__Schema_Product_Supplier.sql
+++ src/main/resources/db/migration/V20220208__Schema_Product_Supplier.sql
@@ -1,11 +1,7 @@
 create table product_supplier (
     id_product character varying(64) not null,
-    id_supplier character varying(64) not null
+    id_supplier character varying(64) not null,
+    constraint product_supplier_pkey primary key (id_product, id_supplier),
+    constraint fk_product foreign key (id_product) references products(id),
+    constraint fk_supplier foreign key (id_supplier) references suppliers(id)
 );
-
-alter table product_supplier
-    add constraint product_supplier_pkey primary key (id_product, id_supplier);
-alter table product_supplier
-    add constraint fk_product foreign key (id_product) references products(id);
-alter table product_supplier
-    add constraint fk_supplier foreign key (id_supplier) references suppliers(id);
