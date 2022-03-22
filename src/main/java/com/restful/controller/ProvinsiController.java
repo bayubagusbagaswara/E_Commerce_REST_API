cb19509239db0d4617c1e728fce4552b25b8b1a6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-22 11:19:22 +0700

    add api getAllProvinsi in ProvinsiController

--- src/main/java/com/restful/controller/ProvinsiController.java
+++ src/main/java/com/restful/controller/ProvinsiController.java
@@ -2,9 +2,12 @@ package com.restful.controller;
 
 import com.restful.dto.WebResponseDto;
 import com.restful.dto.provinsi.CreateProvinsiRequestDto;
+import com.restful.dto.provinsi.ListProvinsiRequestDto;
+import com.restful.dto.provinsi.ListProvinsiResponseDto;
 import com.restful.dto.provinsi.ProvinsiResponseDto;
 import com.restful.exception.ProvinsiNotFoundException;
 import com.restful.service.ProvinsiService;
+import com.restful.util.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -41,6 +44,27 @@ public class ProvinsiController {
     }
 
     // Get All Provinsi
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponseDto<ListProvinsiResponseDto> getAllProvinsi(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) Integer pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
+
+        ListProvinsiRequestDto listProvinsiRequest = new ListProvinsiRequestDto();
+        listProvinsiRequest.setPageNo(pageNo);
+        listProvinsiRequest.setPageSize(pageSize);
+        listProvinsiRequest.setSortBy(sortBy);
+        listProvinsiRequest.setSortDir(sortDir);
+
+        final ListProvinsiResponseDto allProvinsiResponse = provinsiService.getAllProvinsi(listProvinsiRequest);
+        return WebResponseDto.<ListProvinsiResponseDto>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(allProvinsiResponse)
+                .build();
+    }
+
     // Update Provinsi
     // Delete Provinsi
 }
