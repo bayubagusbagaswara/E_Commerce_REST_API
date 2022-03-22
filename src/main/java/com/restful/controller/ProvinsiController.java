a6a99e8e0bbd49edf64afe623ef460b993f5d1bb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-22 11:12:52 +0700

    add api getProvinsiById in ProvinsiController

--- src/main/java/com/restful/controller/ProvinsiController.java
+++ src/main/java/com/restful/controller/ProvinsiController.java
@@ -3,13 +3,11 @@ package com.restful.controller;
 import com.restful.dto.WebResponseDto;
 import com.restful.dto.provinsi.CreateProvinsiRequestDto;
 import com.restful.dto.provinsi.ProvinsiResponseDto;
+import com.restful.exception.ProvinsiNotFoundException;
 import com.restful.service.ProvinsiService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/provinsi")
@@ -32,6 +30,16 @@ public class ProvinsiController {
     }
 
     // Get Provinsi By ID, /{idProvinsi}
+    @GetMapping(value = "/{idProvinsi}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponseDto<ProvinsiResponseDto> getProvinsiById(@PathVariable("idProvinsi") String id) throws ProvinsiNotFoundException {
+        final ProvinsiResponseDto provinsiResponse = provinsiService.getProvinsiById(id);
+        return WebResponseDto.<ProvinsiResponseDto>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(provinsiResponse)
+                .build();
+    }
+
     // Get All Provinsi
     // Update Provinsi
     // Delete Provinsi
