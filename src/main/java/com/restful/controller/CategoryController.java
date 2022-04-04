9cc599ef7272e82e877dafb8b1b3b162d2b2b81c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-04 12:38:17 +0700

    update controller CategoryController

--- src/main/java/com/restful/controller/CategoryController.java
+++ src/main/java/com/restful/controller/CategoryController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
@@ -19,8 +21,6 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    // consumes = Content-Type (atau yang dikirim dari request user) atau ini yang dikonsumsi oleh API
-    // produces = Accept (yang dikirim dari server berupa response json) atau ini hasil (produksi) dari API
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public WebResponseDto<CategoryResponseDto> createCategory(@RequestBody CreateCategoryRequestDto createCategoryRequest) {
         CategoryResponseDto categoryResponse = categoryService.createCategory(createCategoryRequest);
@@ -31,7 +31,6 @@ public class CategoryController {
                 .build();
     }
 
-    // get Category By ID
     @GetMapping(value = "/{idCategory}", produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponseDto<CategoryResponseDto> getCategoryById(@PathVariable("idCategory") String id) throws CategoryNotFoundException {
         final CategoryResponseDto categoryResponse = categoryService.getCategoryById(id);
@@ -42,7 +41,6 @@ public class CategoryController {
                 .build();
     }
 
-    // get All Category
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponseDto<ListCategoryResponseDto> getAllCategories(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) Integer pageNo,
@@ -64,7 +62,6 @@ public class CategoryController {
                 .build();
     }
 
-    // update Category
     @PutMapping(value = "/{idCategory}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public WebResponseDto<CategoryResponseDto> updateCategory(@PathVariable("idCategory") String id, @RequestBody UpdateCategoryRequestDto updateCategoryRequest) throws CategoryNotFoundException {
         final CategoryResponseDto categoryResponse = categoryService.updateCategory(id, updateCategoryRequest);
@@ -75,7 +72,6 @@ public class CategoryController {
                 .build();
     }
 
-    // delete Category
     @DeleteMapping(value = "/{idCategory}", produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponseDto<String> deleteCategory(@PathVariable("idCategory") String id) {
         categoryService.deleteCategory(id);
@@ -86,5 +82,43 @@ public class CategoryController {
                 .build();
     }
 
-    // get CategoryBy Name Containing (ini harusnya bisa dengan endpoint /categorie atau getAllCategories)
+    @GetMapping(value = "/name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponseDto<CategoryResponseDto> getCategoryByName(@RequestParam("name") String name) throws CategoryNotFoundException {
+        final CategoryResponseDto categoryResponse = categoryService.getCategoryByName(name);
+        return WebResponseDto.<CategoryResponseDto>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(categoryResponse)
+                .build();
+    }
+
+    @GetMapping(value = "/name/contains", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponseDto<List<CategoryResponseDto>> getCategoryByNameContains(@RequestParam("name") String name) {
+        final List<CategoryResponseDto> categoryResponseList = categoryService.getCategoryByNameContains(name);
+        return WebResponseDto.<List<CategoryResponseDto>>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(categoryResponseList)
+                .build();
+    }
+
+    @GetMapping(value = "/name/starting", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponseDto<List<CategoryResponseDto>> getCategoryByNameStartsWith(@RequestParam("name") String name) {
+        final List<CategoryResponseDto> categoryResponseList = categoryService.getCategoryStartingWithName(name);
+        return WebResponseDto.<List<CategoryResponseDto>>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(categoryResponseList)
+                .build();
+    }
+
+    @GetMapping(value = "/product/{idProduct}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponseDto<CategoryResponseDto> getCategoryByProductId(@PathVariable("idProduct") String id) throws CategoryNotFoundException {
+        final CategoryResponseDto categoryResponse = categoryService.getCategoryByProductId(id);
+        return WebResponseDto.<CategoryResponseDto>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(categoryResponse)
+                .build();
+    }
 }
