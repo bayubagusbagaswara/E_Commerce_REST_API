b5cbc395466c043b79bad57bc575c43d850dec49
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-31 21:52:44 +0700

    add WishListService

---
+++ src/main/java/com/ecommerce/service/WishListService.java
@@ -0,0 +1,73 @@
+package com.ecommerce.service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class WishListService {
+
+    private final WishListRepository wishListRepository;
+    private final WishListItemRepository wishListItemRepository;
+    private final ProductService productService;
+
+    public WishList addToWishFirstTime(Long id, String sessionToken) {
+        WishList wishlist = new WishList();
+        WishListItem item = new WishListItem();
+
+        item.setDate(new Date());
+        item.setProduct(productService.getProductById(id));
+        wishlist.getItems().add(item);
+        wishlist.setSessionToken(sessionToken);
+        wishlist.setDate(new Date());
+        return wishListRepository.save(wishlist);
+    }
+
+    public WishList addToExistingWishList(Long id, String sessionToken) {
+        WishList wishList = wishListRepository.findBySessionToken(sessionToken);
+        Product p = productService.getProductById(id);
+        Boolean productDoesExistInTheCart = false;
+        if (wishList != null) {
+            Set<WishListItem> items = wishList.getItems();
+            for (WishListItem item : items) {
+                if (item.getProduct().equals(p)) {
+                    productDoesExistInTheCart = true;
+                    break;
+                }
+
+            }
+        }
+        if (!productDoesExistInTheCart && (wishList != null)) {
+            WishListItem item1 = new WishListItem();
+            item1.setDate(new Date());
+            item1.setProduct(p);
+            wishList.getItems().add(item1);
+            return wishListRepository.saveAndFlush(wishList);
+        }
+
+        return null;
+
+    }
+
+    public WishList getWishListBySessionToken(String sessionToken) {
+        return wishListRepository.findBySessionToken(sessionToken);
+    }
+
+    public WishList removeItemWishList(Long id, String sessionToken) {
+        WishList WishList = wishListRepository.findBySessionToken(sessionToken);
+        Set<WishListItem> items = WishList.getItems();
+        WishListItem item = null;
+        for (WishListItem item1 : items) {
+            if (item1.getId() == id) {
+                item = item1;
+            }
+        }
+        items.remove(item);
+        wishListItemRepository.delete(item);
+        WishList.setItems(items);
+        return wishListRepository.save(WishList);
+    }
+
+    public void clearWishList(String sessionToken) {
+        WishList sh = wishListRepository.findBySessionToken(sessionToken);
+        wishListRepository.delete(sh);
+    }
+}
