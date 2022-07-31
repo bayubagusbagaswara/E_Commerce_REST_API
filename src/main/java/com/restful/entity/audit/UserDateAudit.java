f71de15d05e12a5432c8d721f8f3962850495ec5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-31 13:26:19 +0700

    update abstract class UserDateAudit

--- src/main/java/com/restful/entity/audit/UserDateAudit.java
+++ src/main/java/com/restful/entity/audit/UserDateAudit.java
@@ -1,4 +1,28 @@
 package com.restful.entity.audit;
 
-public abstract class UserDateAudit {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EqualsAndHashCode(callSuper = true)
+@Data
+@JsonIgnoreProperties(
+        value = { "createdBy", "updatedBy"},
+        allowGetters = true
+)
+public abstract class UserDateAudit extends DateAudit {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
 }
