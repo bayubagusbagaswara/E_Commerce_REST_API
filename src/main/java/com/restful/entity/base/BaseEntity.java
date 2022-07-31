f464d53d92b597343651dfece96493a9915e6425
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-31 13:27:37 +0700

    update class BaseEntity

--- src/main/java/com/restful/entity/base/BaseEntity.java
+++ src/main/java/com/restful/entity/base/BaseEntity.java
@@ -1,43 +1,23 @@
 package com.restful.entity.base;
 
+import com.restful.entity.audit.UserDateAudit;
 import com.restful.entity.enumerator.StatusRecord;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@EntityListeners(AuditingEntityListener.class)
-@MappedSuperclass
 @Data
-public class BaseEntity {
+@EqualsAndHashCode(callSuper = true)
+public class BaseEntity extends UserDateAudit {
 
     @Id
     @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     @Column(name = "id", nullable = false, length = 64)
     private String id;
-
-    @CreatedBy
-    @Column(name = "created_by", nullable = false)
-    private String createdBy;
-
-    @CreatedDate
-    @Column(name = "created_date", nullable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedBy
-    @Column(name = "updated_by")
-    private String updatedBy;
-
-    @LastModifiedDate
-    @Column(name = "updated_date")
-    private LocalDateTime updatedDate;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status_record", nullable = false)
