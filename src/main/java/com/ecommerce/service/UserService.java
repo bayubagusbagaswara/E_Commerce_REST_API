c9a961fa8565f889b019bf0f60101330eba6d84d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-31 20:43:13 +0700

    ad contract UserService

---
+++ src/main/java/com/ecommerce/service/UserService.java
@@ -0,0 +1,14 @@
+package com.ecommerce.service;
+
+public interface UserService {
+
+    // create new user
+
+    // get user by id
+
+    // get all user
+
+    // update user
+
+    // delete user
+}
