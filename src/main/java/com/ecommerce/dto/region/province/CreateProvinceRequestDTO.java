b516807d9e2fef5b756026d4ab793ae58c1012e5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-07-31 20:53:19 +0700

    update dto CreateProvinceRequestDTO

--- src/main/java/com/ecommerce/dto/region/province/CreateProvinceRequestDTO.java
+++ src/main/java/com/ecommerce/dto/region/province/CreateProvinceRequestDTO.java
@@ -1,10 +1,12 @@
 package com.ecommerce.dto.region.province;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class CreateProvinceRequestDTO {
 
