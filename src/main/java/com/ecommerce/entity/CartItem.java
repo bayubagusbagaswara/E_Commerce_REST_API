0408db72ace649168c2ea9f6b2bb4de6c11b050f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-02 07:57:23 +0700

    update entity CartItem

--- src/main/java/com/ecommerce/entity/CartItem.java
+++ src/main/java/com/ecommerce/entity/CartItem.java
@@ -1,24 +1,65 @@
 package com.ecommerce.entity;
 
-import lombok.Data;
+import com.ecommerce.entity.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDate;
 
+/**
+ * Cart item berisi semua data product/item yang akan kita masukkan kedalam keranjang belanja consumer
+ * Misal Product MacBook Pro, kita mau beli sebanyak 5
+ * Maka di table CartItem akan ada data sebagai berikut:
+ * product_id = MacBook Pro
+ * quantity = 5
+ * date = tanggal cart item nya dibuat
+ * Jadi cart item ini belum masuk ke menu keranjang, tetapi masih memilih product beserta jumlahnya
+ */
 @Entity
+@Table(name = "cart_items")
 @Data
-public class CartItem {
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+    @Column(name = "quantity")
     private int quantity;
 
-    @Temporal(TemporalType.DATE)
-    private Date date;
+    @Column(name = "date")
+    private LocalDate date;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @FieldNameConstants.Exclude
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "product_id", nullable = false, updatable = false)
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "fk_cart_item_product_id"), nullable = false, updatable = false)
     private Product product;
+
+    @JsonIgnore
+    @Override
+    public Instant getCreatedAt() {
+        return super.getCreatedAt();
+    }
+
+    @JsonIgnore
+    @Override
+    public Instant getUpdatedAt() {
+        return super.getUpdatedAt();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getCreatedBy() {
+        return super.getCreatedBy();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getUpdatedBy() {
+        return super.getUpdatedBy();
+    }
 }
