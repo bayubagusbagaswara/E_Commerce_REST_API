cc7edeb27333c2f0db377cfceabc228b3728ac31
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 16:05:15 +0700

    update SupplierRepository

--- src/main/java/com/ecommerce/repository/SupplierRepository.java
+++ src/main/java/com/ecommerce/repository/SupplierRepository.java
@@ -2,21 +2,19 @@ package com.ecommerce.repository;
 
 import com.ecommerce.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
 
-    // get supplier by name ignore case
     Optional<Supplier> findAllByNameIgnoreCase(String name);
 
-    // get supplier by email
     Optional<Supplier> findAllByEmail(String email);
 
-    // get all supplier by name containing ignore case
     List<Supplier> findAllByNameContainingIgnoreCase(String name);
 
-    // get all supplier by product id
     List<Supplier> findAllByProductsId(String productId);
 }
