d33ec453282eeefa54045e2eae411ba94961bda4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 15:38:18 +0700

    rename entity WishListItem

--- src/main/java/com/ecommerce/entity/WishListItem.java
+++ src/main/java/com/ecommerce/entity/WishListItem.java
@@ -1,21 +1,26 @@
 package com.ecommerce.entity;
 
+import com.ecommerce.entity.base.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
+@Table(name = "wish_list_items")
 @Data
-public class WishListItem {
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class WishListItem extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name = "date")
+    private LocalDate date;
 
-    @Temporal(TemporalType.DATE)
-    private Date date;
-
+    // 1 Product bisa memiliki WishList yang banyak
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "product_id", nullable = false, updatable = false)
     private Product product;
