6d44a54f7c747bb6b0f3684866873a76fd3743ca
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 08:25:59 +0700

    update entity UserPassword

--- src/main/java/com/ecommerce/entity/user/UserPassword.java
+++ src/main/java/com/ecommerce/entity/user/UserPassword.java
@@ -1,7 +1,27 @@
 package com.ecommerce.entity.user;
 
-/**
- * relasi one to one dengan class User
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_password")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserPassword {
+
+    @Id
+    @Column(name = "user_id")
+    private String id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_user_password_user_id"))
+    private User user;
+
+    @Column(name = "password")
+    private String password;
 }
