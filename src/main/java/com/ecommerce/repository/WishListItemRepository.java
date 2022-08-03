ec3abcbd1b8811b8f7a1af82eca6bf6264a20780
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 16:05:36 +0700

    update WishListItemRepository

--- src/main/java/com/ecommerce/repository/WishListItemRepository.java
+++ src/main/java/com/ecommerce/repository/WishListItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WishListItemRepository extends JpaRepository<WishListItem, Long> {
+public interface WishListItemRepository extends JpaRepository<WishListItem, String> {
 }
