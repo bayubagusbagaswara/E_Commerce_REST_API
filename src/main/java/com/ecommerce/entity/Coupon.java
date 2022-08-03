65560a044df80d9c986a56e1857341d29eeb2e20
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 08:24:31 +0700

    update entity Coupon

--- src/main/java/com/ecommerce/entity/Coupon.java
+++ src/main/java/com/ecommerce/entity/Coupon.java
@@ -1,19 +1,56 @@
 package com.ecommerce.entity;
 
+import com.ecommerce.entity.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.Instant;
 
+/**
+ * Table Kupon ini berisi tentang nilai diskon dari sebuah produk
+ * Jadi diskon ini akan berelasi dengan produk
+ * Biasanya relasinya adalah one to one, karena 1 product hanya memiliki 1 kupon diskon
+ */
 @Entity
+@Table(name = "coupons")
 @Data
-public class Coupon {
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+    @Column(name = "discount")
     private int discount;
 
+    // ini hanya untuk cek apakah diskon tersedia atau tidak
     @Transient
     private boolean isDiscount;
+
+    @JsonIgnore
+    @Override
+    public Instant getCreatedAt() {
+        return super.getCreatedAt();
+    }
+
+    @JsonIgnore
+    @Override
+    public Instant getUpdatedAt() {
+        return super.getUpdatedAt();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getCreatedBy() {
+        return super.getCreatedBy();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getUpdatedBy() {
+        return super.getUpdatedBy();
+    }
 }
