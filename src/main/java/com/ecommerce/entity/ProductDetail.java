07ec6056e872b8c31189cdd0abda3e435b961a33
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 16:40:09 +0700

    update entity ProductDetail

--- src/main/java/com/ecommerce/entity/ProductDetail.java
+++ src/main/java/com/ecommerce/entity/ProductDetail.java
@@ -1,28 +1,21 @@
 package com.ecommerce.entity;
 
-import com.ecommerce.entity.base.BaseEntity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import java.time.Instant;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "product_details", uniqueConstraints = {
         @UniqueConstraint(name = "product_detail_unique_sku", columnNames = "sku")
 })
 @Data
-@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE product_detail SET status_record = 'INACTIVE' WHERE id = ?")
-@Where(clause = "status_record = 'ACTIVE'")
-public class ProductDetail extends BaseEntity {
+public class ProductDetail {
+
+    @Id
+    @Column(name = "product_id")
+    private String id;
 
     @Column(name = "sku", nullable = false, length = 50)
     private String sku;
@@ -30,35 +23,8 @@ public class ProductDetail extends BaseEntity {
     @Column(name = "description", nullable = false, length = 500)
     private String description;
 
-    @Override
-    public void setUpdatedAt(Instant updatedAt) {
-        super.setUpdatedAt(updatedAt);
-    }
-
-    @Override
-    public void setCreatedAt(Instant createdAt) {
-        super.setCreatedAt(createdAt);
-    }
-
-    @Override
-    public Instant getCreatedAt() {
-        return super.getCreatedAt();
-    }
-
-    @Override
-    public Instant getUpdatedAt() {
-        return super.getUpdatedAt();
-    }
-
-    @JsonIgnore
-    @Override
-    public String getCreatedBy() {
-        return super.getCreatedBy();
-    }
-
-    @JsonIgnore
-    @Override
-    public String getUpdatedBy() {
-        return super.getUpdatedBy();
-    }
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "fk_product_detail_product_id"))
+    private Product product;
 }
