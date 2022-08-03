e0eb350c052460a2866458520c1f99b2acc8e541
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 16:07:29 +0700

    update ProductDetailRepository

--- src/main/java/com/ecommerce/repository/ProductDetailRepository.java
+++ src/main/java/com/ecommerce/repository/ProductDetailRepository.java
@@ -2,11 +2,12 @@ package com.ecommerce.repository;
 
 import com.ecommerce.entity.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, String> {
 
-    // get product detail by SKU
     Optional<ProductDetail> findAllBySku(String sku);
 }
