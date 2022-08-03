912b04b370ee9fe4362f3a361bd1cb6b07b3fea8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 15:36:21 +0700

    rename entity WishList

--- src/main/java/com/ecommerce/entity/WishList.java
+++ src/main/java/com/ecommerce/entity/WishList.java
@@ -1,25 +1,63 @@
 package com.ecommerce.entity;
 
+import com.ecommerce.entity.base.BaseEntity;
+import com.ecommerce.entity.enumerator.StatusRecord;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "wish_lists")
 @Data
-public class WishList {
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class WishList extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name = "date")
+    private LocalDate date;
 
-    @Temporal(TemporalType.DATE)
-    private Date date;
-
+    @Column(name = "session_token")
     private String sessionToken;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Set<WishListItem> items = new HashSet<>();
+
+    @JsonIgnore
+    @Override
+    public Instant getCreatedAt() {
+        return super.getCreatedAt();
+    }
+
+    @JsonIgnore
+    @Override
+    public Instant getUpdatedAt() {
+        return super.getUpdatedAt();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getCreatedBy() {
+        return super.getCreatedBy();
+    }
+
+    @JsonIgnore
+    @Override
+    public String getUpdatedBy() {
+        return super.getUpdatedBy();
+    }
+
+    @JsonIgnore
+    @Override
+    public StatusRecord getStatusRecord() {
+        return super.getStatusRecord();
+    }
 }
