4d42c08d036d6cf7b9c55afcfec13c426aa90ef8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 16:04:22 +0700

    update ShoppingCartRepository

--- src/main/java/com/ecommerce/repository/ShoppingCartRepository.java
+++ src/main/java/com/ecommerce/repository/ShoppingCartRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, String> {
 
     ShoppingCart findBySessionToken(String sessionToken);
 }
