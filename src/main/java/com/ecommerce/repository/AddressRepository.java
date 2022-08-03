cce976b6a7221baf4ac479958aeade87b5a6808e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-03 16:06:32 +0700

    update AddressRepository

--- src/main/java/com/ecommerce/repository/AddressRepository.java
+++ src/main/java/com/ecommerce/repository/AddressRepository.java
@@ -2,11 +2,9 @@ package com.ecommerce.repository;
 
 import com.ecommerce.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
 
-    // find by postal code
-    Optional<Address> findAllByPostalCode(String postalCode);
 }
