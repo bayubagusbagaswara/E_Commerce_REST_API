6a71f41599d8a002ec4df46951b8bad6424b5fc7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 09:00:09 +0700

    add SubDistrictMapper

---
+++ src/main/java/com/ecommerce/mapper/region/SubDistrictMapper.java
@@ -0,0 +1,19 @@
+package com.ecommerce.mapper.region;
+
+import com.ecommerce.dto.region.subDistrict.SubDistrictDTO;
+import com.ecommerce.entity.region.SubDistrict;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubDistrictMapper {
+
+    public SubDistrictDTO fromSubDistrict(SubDistrict subDistrict) {
+        SubDistrictDTO dto = new SubDistrictDTO();
+        dto.setId(subDistrict.getId());
+        dto.setCode(subDistrict.getCode());
+        dto.setName(subDistrict.getName());
+        dto.setCreatedAt(subDistrict.getCreatedAt());
+        dto.setDistrict(subDistrict.getDistrict());
+        return dto;
+    }
+}
