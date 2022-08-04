7c8a46f7bd45d8ca78b1b82cd313544e0fb40838
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 14:11:06 +0700

    update ProductRepository

--- src/main/java/com/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/repository/ProductRepository.java
@@ -9,29 +9,17 @@ import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-    // get product by name ignore case
     Optional<Product> findProductByNameIgnoreCase(String name);
+    List<Product> findAllByNameContainingIgnoreCase(String name);
+    Optional<Product> findAllByProductDetailSku(String sku);
+    List<Product> findAllByPriceBetween(BigDecimal priceMin, BigDecimal priceMax);
+    List<Product> findAllByNameContainingAndPriceBetween(String name, BigDecimal priceMin, BigDecimal priceMax);
+    List<Product> findAllByCategoryId(String categoryId);
+    List<Product> findAllBySuppliersId(String supplierId);
 
-//    List<Product> findByNameContainingIgnoreCase(String name);
+    //    List<Product> findByNameContainingIgnoreCase(String name);
 //
 //    @Query("SELECT DISTINCT brand FROM Product")
 //    List<String> findAllBrandsDistincts();
 
-    // get product by name containing ignore case
-    List<Product> findAllByNameContainingIgnoreCase(String name);
-
-    // get product by sku
-    Optional<Product> findAllByProductDetailSku(String sku);
-
-    // get product by price between
-    List<Product> findAllByPriceBetween(BigDecimal priceMin, BigDecimal priceMax);
-
-    // get product by name containing and price between
-    List<Product> findAllByNameContainingAndPriceBetween(String name, BigDecimal priceMin, BigDecimal priceMax);
-
-    // get product by category id
-    List<Product> findAllByCategoryId(String categoryId);
-
-    // get product by supplier id
-    List<Product> findAllBySuppliersId(String supplierId);
 }
