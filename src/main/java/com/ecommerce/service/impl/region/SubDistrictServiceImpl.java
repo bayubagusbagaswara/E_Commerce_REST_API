b0d69bf97863c86cb65e7d60629c30c1708c804f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 20:15:49 +0700

    rename and update all method SubDistrictServiceImpl

--- src/main/java/com/ecommerce/service/impl/region/SubDistrictServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/region/SubDistrictServiceImpl.java
@@ -2,13 +2,14 @@ package com.ecommerce.service.impl.region;
 
 import com.ecommerce.entity.region.District;
 import com.ecommerce.entity.region.SubDistrict;
-import com.ecommerce.dto.kecamatan.*;
 import com.ecommerce.dto.region.subDistrict.*;
 import com.ecommerce.exception.SubDistrictNotFoundException;
 import com.ecommerce.exception.DistrictNotFoundException;
+import com.ecommerce.mapper.region.SubDistrictMapper;
 import com.ecommerce.repository.region.SubDistrictRepository;
 import com.ecommerce.repository.region.DistrictRepository;
 import com.ecommerce.service.region.SubDistrictService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -25,100 +26,94 @@ public class SubDistrictServiceImpl implements SubDistrictService {
 
     private final SubDistrictRepository subDistrictRepository;
     private final DistrictRepository districtRepository;
-    private final RegionMapper regionMapper;
+    private final SubDistrictMapper subDistrictMapper;
 
-    public SubDistrictServiceImpl(SubDistrictRepository subDistrictRepository, DistrictRepository districtRepository, RegionMapper regionMapper) {
+    @Autowired
+    public SubDistrictServiceImpl(SubDistrictRepository subDistrictRepository, DistrictRepository districtRepository, SubDistrictMapper subDistrictMapper) {
         this.subDistrictRepository = subDistrictRepository;
         this.districtRepository = districtRepository;
-        this.regionMapper = regionMapper;
+        this.subDistrictMapper = subDistrictMapper;
     }
 
     @Override
-    public SubDistrictDTO createKecamatan(CreateSubDistrictRequestDTO createKecamatanRequest) throws DistrictNotFoundException {
-        District district = districtRepository.findById(createKecamatanRequest.getKotaId())
-                .orElseThrow(() -> new DistrictNotFoundException("Kota ID [" + createKecamatanRequest.getKotaId() + "] not found"));
+    public SubDistrictDTO createSubDistrict(CreateSubDistrictRequestDTO createSubDistrictRequestDTO) {
+        District district = districtRepository.findById(createSubDistrictRequestDTO.getDistrictId())
+                .orElseThrow(() -> new DistrictNotFoundException("id", createSubDistrictRequestDTO.getDistrictId()));
         SubDistrict subDistrict = new SubDistrict();
-        subDistrict.setCode(createKecamatanRequest.getCode());
-        subDistrict.setName(createKecamatanRequest.getName());
+        subDistrict.setCode(createSubDistrictRequestDTO.getCode());
+        subDistrict.setName(createSubDistrictRequestDTO.getName());
         subDistrict.setDistrict(district);
-        subDistrict.setCreatedDate(LocalDateTime.now());
+        subDistrict.setCreatedAt(Instant.now());
         subDistrictRepository.save(subDistrict);
-        return regionMapper.mapToKecamatanResponse(subDistrict);
+        return subDistrictMapper.fromSubDistrict(subDistrict);
     }
 
     @Override
-    public SubDistrictDTO getKecamatanById(String id) throws SubDistrictNotFoundException {
-        SubDistrict subDistrict = subDistrictRepository.findById(id)
-                .orElseThrow(() -> new SubDistrictNotFoundException("Kecamatan ID [" + id + "] not found"));
-        return regionMapper.mapToKecamatanResponse(subDistrict);
+    public SubDistrictDTO getSubDistrictById(String subDistrictId) {
+        SubDistrict subDistrict = subDistrictRepository.findById(subDistrictId)
+                .orElseThrow(() -> new SubDistrictNotFoundException("id", subDistrictId));
+        return subDistrictMapper.fromSubDistrict(subDistrict);
     }
 
     @Override
-    public SubDistrictDTO updateKecamatan(String id, UpdateSubDistrictRequestDTO updateKecamatanRequest) throws DistrictNotFoundException, SubDistrictNotFoundException {
-        District district = districtRepository.findById(updateKecamatanRequest.getKotaId())
-                .orElseThrow(() -> new DistrictNotFoundException("Kota ID [" + updateKecamatanRequest.getKotaId() + "] not found"));
-        SubDistrict subDistrict = subDistrictRepository.findById(id)
-                .orElseThrow(() -> new SubDistrictNotFoundException("Kecamatan ID [" + id + "] not found"));
-        subDistrict.setCode(updateKecamatanRequest.getCode());
-        subDistrict.setName(updateKecamatanRequest.getName());
+    public ListSubDistrictResponseDTO getAllSubDistricts(ListSubDistrictRequestDTO listSubDistrictRequestDTO) {
+        Integer pageNo = listSubDistrictRequestDTO.getPageNo();
+        Integer pageSize = listSubDistrictRequestDTO.getPageSize();
+        String sortBy = listSubDistrictRequestDTO.getSortBy();
+        String sortDir = listSubDistrictRequestDTO.getSortDir();
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Page<SubDistrict> subDistrictPage = subDistrictRepository.findAll(pageable);
+        List<SubDistrict> subDistrictList = subDistrictPage.getContent();
+
+        List<SubDistrictDTO> subDistrictDTOList = subDistrictMapper.fromSubDistrictList(subDistrictList);
+
+        return new ListSubDistrictResponseDTO(
+                subDistrictDTOList, subDistrictPage.getNumber(), subDistrictPage.getSize(),
+                subDistrictPage.getTotalElements(), subDistrictPage.getTotalPages(), subDistrictPage.isLast()
+        );
+    }
+
+    @Override
+    public SubDistrictDTO updateSubDistrict(String subDistrictId, UpdateSubDistrictRequestDTO updateSubDistrictRequestDTO) {
+        District district = districtRepository.findById(updateSubDistrictRequestDTO.getDistrictId()).orElseThrow(() -> new DistrictNotFoundException("id", updateSubDistrictRequestDTO.getDistrictId()));
+        SubDistrict subDistrict = subDistrictRepository.findById(subDistrictId).orElseThrow(() -> new SubDistrictNotFoundException("id", subDistrictId));
+        subDistrict.setCode(updateSubDistrictRequestDTO.getCode());
+        subDistrict.setName(updateSubDistrictRequestDTO.getName());
         subDistrict.setDistrict(district);
-        subDistrict.setUpdatedDate(LocalDateTime.now());
+        subDistrict.setUpdatedAt(Instant.now());
         subDistrictRepository.save(subDistrict);
-        return regionMapper.mapToKecamatanResponse(subDistrict);
+        return subDistrictMapper.fromSubDistrict(subDistrict);
     }
 
     @Override
-    public void deleteKecamatan(String id) throws SubDistrictNotFoundException {
-        final SubDistrict subDistrict = subDistrictRepository.findById(id)
-                .orElseThrow(() -> new SubDistrictNotFoundException("Kecamatan ID [" + id + "] not found"));
+    public void deleteSubDistrict(String subDistrictId) {
+        final SubDistrict subDistrict = subDistrictRepository.findById(subDistrictId).orElseThrow(() -> new SubDistrictNotFoundException("id", subDistrictId));
         subDistrictRepository.delete(subDistrict);
     }
 
     @Override
-    public ListSubDistrictResponseDTO getAllKecamatan(ListSubDistrictRequestDTO listKecamatanRequest) {
-        Integer pageNo = listKecamatanRequest.getPageNo();
-        Integer pageSize = listKecamatanRequest.getPageSize();
-        String sortBy = listKecamatanRequest.getSortBy();
-        String sortDir = listKecamatanRequest.getSortDir();
-
-        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
-        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
-        Page<SubDistrict> kecamatanPage = subDistrictRepository.findAll(pageable);
-        List<SubDistrict> subDistrictList = kecamatanPage.getContent();
-
-        List<SubDistrictDTO> kecamatanResponseList = regionMapper.mapToKecamatanResponseList(subDistrictList);
-
-        ListSubDistrictResponseDTO listKecamatanResponse = new ListSubDistrictResponseDTO();
-        listKecamatanResponse.setKecamatanList(kecamatanResponseList);
-        listKecamatanResponse.setPageNo(kecamatanPage.getNumber());
-        listKecamatanResponse.setPageSize(kecamatanPage.getSize());
-        listKecamatanResponse.setTotalElements(kecamatanPage.getTotalElements());
-        listKecamatanResponse.setTotalPages(kecamatanPage.getTotalPages());
-        listKecamatanResponse.setLast(kecamatanPage.isLast());
-        return listKecamatanResponse;
+    public SubDistrictDTO getSubDistrictByName(String name) {
+        SubDistrict subDistrict = subDistrictRepository.findAllByNameIgnoreCase(name).orElseThrow(() -> new SubDistrictNotFoundException("name", name));
+        return subDistrictMapper.fromSubDistrict(subDistrict);
     }
 
     @Override
-    public SubDistrictDTO getKecamatanByName(String name) throws SubDistrictNotFoundException {
-        SubDistrict subDistrict = subDistrictRepository.findAllByNameIgnoreCase(name).orElseThrow(() -> new SubDistrictNotFoundException("Kecamatan name [" + name + "] not found"));
-        return regionMapper.mapToKecamatanResponse(subDistrict);
-    }
-
-    @Override
-    public SubDistrictDTO getKecamatanByCode(String code) throws SubDistrictNotFoundException {
-        SubDistrict subDistrict = subDistrictRepository.findAllByCode(code).orElseThrow(() -> new SubDistrictNotFoundException("Kecamatan code [" + code + "] not found"));
-        return regionMapper.mapToKecamatanResponse(subDistrict);
-    }
-
-    @Override
-    public List<SubDistrictDTO> getKecamatanByNameContains(String name) {
+    public List<SubDistrictDTO> getSubDistrictByNameContains(String name) {
         List<SubDistrict> subDistrictList = subDistrictRepository.findAllByNameContainingIgnoreCase(name);
-        return regionMapper.mapToKecamatanResponseList(subDistrictList);
+        return subDistrictMapper.fromSubDistrictList(subDistrictList);
     }
 
     @Override
-    public List<SubDistrictDTO> getKecamatanByKotaId(String kotaId) {
-        List<SubDistrict> subDistrictList = subDistrictRepository.findAllByKotaId(kotaId);
-        return regionMapper.mapToKecamatanResponseList(subDistrictList);
+    public SubDistrictDTO getSubDistrictByCode(String code) {
+        SubDistrict subDistrict = subDistrictRepository.findAllByCode(code).orElseThrow(() -> new SubDistrictNotFoundException("code", code));
+        return subDistrictMapper.fromSubDistrict(subDistrict);
+    }
+
+    @Override
+    public List<SubDistrictDTO> getAllSubDistrictsByDistrictId(String districtId) {
+        List<SubDistrict> subDistrictList = subDistrictRepository.findAllByDistrictId(districtId);
+        return subDistrictMapper.fromSubDistrictList(subDistrictList);
     }
 }
