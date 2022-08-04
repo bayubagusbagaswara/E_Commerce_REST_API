51ea822cb3a38e13bcf8b679140f5b6709cb7f55
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 14:06:54 +0700

    update all class exception

--- src/main/java/com/ecommerce/exception/ProvinceNotFoundException.java
+++ src/main/java/com/ecommerce/exception/ProvinceNotFoundException.java
@@ -1,11 +1,13 @@
 package com.ecommerce.exception;
 
 import com.ecommerce.dto.ApiResponse;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.Transient;
 
+@Setter
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProvinceNotFoundException extends RuntimeException {
     @Transient
