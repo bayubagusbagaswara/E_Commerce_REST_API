4d80b75de3208054daa9c23dc5267fe983e61342
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 20:32:20 +0700

    add constructor

--- src/main/java/com/ecommerce/dto/region/urbanVillage/ListUrbanVillageResponseDTO.java
+++ src/main/java/com/ecommerce/dto/region/urbanVillage/ListUrbanVillageResponseDTO.java
@@ -1,11 +1,13 @@
 package com.ecommerce.dto.region.urbanVillage;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ListUrbanVillageResponseDTO {
 
