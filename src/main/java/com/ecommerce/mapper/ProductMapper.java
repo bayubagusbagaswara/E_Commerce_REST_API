cd13ccb7bb35b15224ee523bd6c29ae1f73a21aa
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 09:09:52 +0700

    add ProductMapper

--- src/main/java/com/ecommerce/mapper/ProductMapper.java
+++ src/main/java/com/ecommerce/mapper/ProductMapper.java
@@ -1,9 +1,7 @@
 package com.ecommerce.mapper;
 
 import com.ecommerce.dto.product.ProductDTO;
-import com.ecommerce.dto.supplier.SupplierDTO;
 import com.ecommerce.entity.Product;
-import com.ecommerce.entity.Supplier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,20 +10,7 @@ import java.util.stream.Collectors;
 @Component
 public class ProductMapper {
 
-    public SupplierDTO fromEntity(Supplier supplier) {
-        SupplierDTO supplierDTO = new SupplierDTO();
-        supplierDTO.setId(supplier.getId());
-        supplierDTO.setName(supplier.getName());
-        supplierDTO.setEmail(supplier.getEmail());
-        supplierDTO.setGender(supplier.getGender());
-        supplierDTO.setMobilePhone(supplier.getMobilePhone());
-        supplierDTO.setSupplierAddress(supplier.getSupplierAddress());
-        supplierDTO.setCreatedAt(supplier.getCreatedAt());
-        supplierDTO.setProductSet(supplier.getProducts());
-        return supplierDTO;
-    }
-
-    public ProductDTO fromEntity(Product product) {
+    public ProductDTO fromProduct(Product product) {
         ProductDTO productDTO = new ProductDTO();
         productDTO.setId(product.getId());
         productDTO.setName(product.getName());
@@ -38,10 +23,9 @@ public class ProductMapper {
         return productDTO;
     }
 
-    public List<ProductDTO> mapToProductResponseList(List<Product> productList) {
+    public List<ProductDTO> fromProductList(List<Product> productList) {
         return productList.stream()
-                .map(this::fromEntity)
-                .collect(Collectors.toList())
-                ;
+                .map(this::fromProduct)
+                .collect(Collectors.toList());
     }
 }
