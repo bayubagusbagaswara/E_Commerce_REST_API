788527e3445b36f6c91f8d5e830adf767a13c6ec
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 08:59:23 +0700

    update SubDistrictDTO

--- src/main/java/com/ecommerce/dto/region/subDistrict/SubDistrictDTO.java
+++ src/main/java/com/ecommerce/dto/region/subDistrict/SubDistrictDTO.java
@@ -1,15 +1,20 @@
 package com.ecommerce.dto.region.subDistrict;
 
-import com.ecommerce.dto.region.district.DistrictDTO;
+import com.ecommerce.entity.region.District;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class SubDistrictDTO {
 
     private String id;
     private String code;
     private String name;
-    private DistrictDTO districtDTO;
+    private Instant createdAt;
+    private District district;
 }
