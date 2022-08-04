1eceeecabe7c742eca9fb8b3262e0f263f3ee278
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-04 14:03:58 +0700

    update ProductNotFoundException

--- src/main/java/com/ecommerce/exception/ProductNotFoundException.java
+++ src/main/java/com/ecommerce/exception/ProductNotFoundException.java
@@ -1,15 +1,44 @@
 package com.ecommerce.exception;
 
+import com.ecommerce.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ProductNotFoundException extends Exception {
+import javax.persistence.Transient;
 
-    public ProductNotFoundException() {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductNotFoundException extends RuntimeException {
+
+    @Transient
+    private transient ApiResponse apiResponse;
+
+    private final String resourceName = "Product";
+    private String fieldName;
+    private Object fieldValue;
+
+    public ProductNotFoundException(String fieldName, Object fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
     }
 
-    public ProductNotFoundException(String message) {
-        super(message);
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
+    public ApiResponse getApiResponse() {
+        return apiResponse;
+    }
+
+    private void setApiResponse() {
+        String message = String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue);
+        apiResponse = new ApiResponse(Boolean.FALSE, message);
     }
 }
