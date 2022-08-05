aa5cde82209b1c57b9dd8074290b71e54fdbf4ce
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-05 20:07:52 +0700

    update SubDistrictNotFoundException

--- src/main/java/com/ecommerce/exception/SubDistrictNotFoundException.java
+++ src/main/java/com/ecommerce/exception/SubDistrictNotFoundException.java
@@ -1,18 +1,18 @@
 package com.ecommerce.exception;
 
-import com.ecommerce.dto.ApiResponse;
-import lombok.Setter;
+import com.ecommerce.dto.MessageResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.Transient;
 
-@Setter
+@AllArgsConstructor
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class SubDistrictNotFoundException extends RuntimeException {
 
     @Transient
-    private transient ApiResponse apiResponse;
+    private transient MessageResponse messageResponse;
 
     private final String resourceName = "Sub District";
     private String fieldName;
@@ -36,12 +36,12 @@ public class SubDistrictNotFoundException extends RuntimeException {
         return fieldValue;
     }
 
-    public ApiResponse getApiResponse() {
-        return apiResponse;
+    public MessageResponse getMessageResponse() {
+        return messageResponse;
     }
 
-    private void setApiResponse() {
+    private void setMessageResponse() {
         String message = String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue);
-        apiResponse = new ApiResponse(Boolean.FALSE, message);
+        messageResponse = new MessageResponse(404, message);
     }
 }
