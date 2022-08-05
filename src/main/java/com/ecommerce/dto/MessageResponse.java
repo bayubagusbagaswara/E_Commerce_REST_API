af92694cfdb06897bf5c6b24cf1b34bf5f416e98
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-05 20:02:39 +0700

    update MessageResponse

--- src/main/java/com/ecommerce/dto/MessageResponse.java
+++ src/main/java/com/ecommerce/dto/MessageResponse.java
@@ -1,36 +1,25 @@
 package com.ecommerce.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Data
 @Builder
 @JsonPropertyOrder({
-        "success",
+        "code",
         "message"
 })
 @AllArgsConstructor
 @NoArgsConstructor
-public class ApiResponse {
+public class MessageResponse {
 
-    @JsonProperty("success")
-    private Boolean success;
+    @JsonProperty("code")
+    private Integer code;
 
     @JsonProperty("message")
     private String message;
-
-    @JsonIgnore
-    private HttpStatus status;
-
-    public ApiResponse(Boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
-
 }
