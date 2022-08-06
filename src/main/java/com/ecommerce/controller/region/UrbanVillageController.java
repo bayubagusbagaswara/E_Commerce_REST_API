e7c4062da6f058ee17fd79de74b84dc426bf481d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-06 13:24:57 +0700

    update all method UrbanVillageController

--- src/main/java/com/ecommerce/controller/region/UrbanVillageController.java
+++ src/main/java/com/ecommerce/controller/region/UrbanVillageController.java
@@ -1,116 +1,79 @@
 package com.ecommerce.controller.region;
 
 import com.ecommerce.dto.WebResponseDTO;
-import com.ecommerce.dto.kelurahan.*;
 import com.ecommerce.dto.region.urbanVillage.*;
-import com.ecommerce.exception.SubDistrictNotFoundException;
-import com.ecommerce.exception.UrbanVillageNotFoundException;
 import com.ecommerce.service.region.UrbanVillageService;
 import com.ecommerce.util.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/urbanvillages")
+@RequestMapping("/api/urban-villages")
 public class UrbanVillageController {
 
     private final UrbanVillageService urbanVillageService;
 
+    @Autowired
     public UrbanVillageController(UrbanVillageService urbanVillageService) {
         this.urbanVillageService = urbanVillageService;
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<UrbanVillageDTO> createKelurahan(@RequestBody CreateUrbanVillageRequestDTO createKelurahanRequest) throws SubDistrictNotFoundException {
-        final UrbanVillageDTO kelurahanResponse = urbanVillageService.createKelurahan(createKelurahanRequest);
-        return WebResponseDTO.<UrbanVillageDTO>builder()
-                .code(HttpStatus.CREATED.value())
-                .status(HttpStatus.CREATED.getReasonPhrase())
-                .data(kelurahanResponse)
-                .build();
+    public ResponseEntity<WebResponseDTO<UrbanVillageDTO>> createUrbanVillage(@RequestBody CreateUrbanVillageRequestDTO createUrbanVillageRequestDTO) {
+        UrbanVillageDTO urbanVillage = urbanVillageService.createUrbanVillage(createUrbanVillageRequestDTO);
+        return new ResponseEntity<>(new WebResponseDTO<>(201, "CREATED", urbanVillage), HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/{idKelurahan}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<UrbanVillageDTO> getKelurahanById(@PathVariable("idKelurahan") String id) throws UrbanVillageNotFoundException {
-        final UrbanVillageDTO kelurahanResponse = urbanVillageService.getKelurahanById(id);
-        return WebResponseDTO.<UrbanVillageDTO>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(kelurahanResponse)
-                .build();
+    @GetMapping(value = "/{urbanVillageId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WebResponseDTO<UrbanVillageDTO>> getUrbanVillageById(@PathVariable(name = "urbanVillageId") String id) {
+        UrbanVillageDTO urbanVillageDTO = urbanVillageService.getUrbanVillageById(id);
+        return new ResponseEntity<>(new WebResponseDTO<>(200, "OK", urbanVillageDTO), HttpStatus.OK);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<ListUrbanVillageResponseDTO> getAllKelurahan(
+    public ResponseEntity<WebResponseDTO<ListUrbanVillageResponseDTO>> getAllUrbanVillages(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) Integer pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
 
-        ListUrbanVillageRequestDTO requestDto = new ListUrbanVillageRequestDTO();
-        requestDto.setPageNo(pageNo);
-        requestDto.setPageSize(pageSize);
-        requestDto.setSortBy(sortBy);
-        requestDto.setSortDir(sortDir);
-
-        final ListUrbanVillageResponseDTO kelurahanResponseList = urbanVillageService.getAllKelurahan(requestDto);
-        return WebResponseDTO.<ListUrbanVillageResponseDTO>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(kelurahanResponseList)
-                .build();
+        ListUrbanVillageRequestDTO listUrbanVillageRequestDTO = new ListUrbanVillageRequestDTO(pageNo, pageSize, sortBy, sortDir);
+        ListUrbanVillageResponseDTO allUrbanVillages = urbanVillageService.getAllUrbanVillages(listUrbanVillageRequestDTO);
+        return new ResponseEntity<>(new WebResponseDTO<>(200, "OK", allUrbanVillages), HttpStatus.OK);
     }
 
-    @PutMapping(value = "/{idKelurahan}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<UrbanVillageDTO> updateKelurahan(@PathVariable("idKelurahan") String id, @RequestBody UpdateUrbanVillageRequestDTO updateKelurahanRequest) throws UrbanVillageNotFoundException, SubDistrictNotFoundException {
-        final UrbanVillageDTO kelurahanResponse = urbanVillageService.updateKelurahan(id, updateKelurahanRequest);
-        return WebResponseDTO.<UrbanVillageDTO>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(kelurahanResponse)
-                .build();
+    @PutMapping(value = "/{urbanVillageId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WebResponseDTO<UrbanVillageDTO>> updateUrbanVillage(@PathVariable(name = "urbanVillageId") String id, @RequestBody UpdateUrbanVillageRequestDTO updateUrbanVillageRequestDTO) {
+        UrbanVillageDTO urbanVillage = urbanVillageService.updateUrbanVillage(id, updateUrbanVillageRequestDTO);
+        return new ResponseEntity<>(new WebResponseDTO<>(201, "CREATED", urbanVillage), HttpStatus.CREATED);
     }
 
-    @DeleteMapping(value = "/{idKelurahan}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<UrbanVillageDTO> deleteKelurahan(@PathVariable("idKelurahan") String id) throws UrbanVillageNotFoundException {
-        urbanVillageService.deleteKelurahan(id);
-        return WebResponseDTO.<UrbanVillageDTO>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(null)
-                .build();
+    @DeleteMapping(value = "/{urbanVillageId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WebResponseDTO<String>> deleteUrbanVillage(@PathVariable(name = "urbanVillageId") String id) {
+        urbanVillageService.deleteUrbanVillage(id);
+        return new ResponseEntity<>(new WebResponseDTO<>(200, "OK", "Successfully Deleted"), HttpStatus.OK);
     }
 
     @GetMapping(value = "/name", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<UrbanVillageDTO> getKelurahanByName(@RequestParam("name") String name) throws UrbanVillageNotFoundException {
-        final UrbanVillageDTO kelurahanResponse = urbanVillageService.getKelurahanByName(name);
-        return WebResponseDTO.<UrbanVillageDTO>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(kelurahanResponse)
-                .build();
+    public ResponseEntity<WebResponseDTO<UrbanVillageDTO>> getUrbanVillageByName(@RequestParam(name = "name") String name) {
+        UrbanVillageDTO urbanVillageDTO = urbanVillageService.getUrbanVillageByName(name);
+        return new ResponseEntity<>(new WebResponseDTO<>(200, "OK", urbanVillageDTO), HttpStatus.OK);
     }
 
     @GetMapping(value = "/name/contains", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<List<UrbanVillageDTO>> getKelurahanByNameContains(@RequestParam("name") String name) {
-        final List<UrbanVillageDTO> kelurahanResponseList = urbanVillageService.getKelurahanByNameContains(name);
-        return WebResponseDTO.<List<UrbanVillageDTO>>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(kelurahanResponseList)
-                .build();
+    public ResponseEntity<WebResponseDTO<List<UrbanVillageDTO>>> getUrbanVillagesByNameContains(@RequestParam(name = "name") String name) {
+        List<UrbanVillageDTO> urbanVillageDTOS = urbanVillageService.getUrbanVillageByNameContains(name);
+        return new ResponseEntity<>(new WebResponseDTO<>(200, "OK", urbanVillageDTOS), HttpStatus.OK);
     }
 
     @GetMapping(value = "/code", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponseDTO<UrbanVillageDTO> getKelurahanByCode(@RequestParam("code") String code) throws UrbanVillageNotFoundException {
-        final UrbanVillageDTO kelurahanResponse = urbanVillageService.getKelurahanByCode(code);
-        return WebResponseDTO.<UrbanVillageDTO>builder()
-                .code(HttpStatus.OK.value())
-                .status(HttpStatus.OK.getReasonPhrase())
-                .data(kelurahanResponse)
-                .build();
+    public ResponseEntity<WebResponseDTO<UrbanVillageDTO>> getUrbanVillageByCode(@RequestParam(name = "code") String code) {
+        UrbanVillageDTO urbanVillageDTO = urbanVillageService.getUrbanVillageByCode(code);
+        return new ResponseEntity<>(new WebResponseDTO<>(200, "OK", urbanVillageDTO), HttpStatus.OK);
     }
 }
