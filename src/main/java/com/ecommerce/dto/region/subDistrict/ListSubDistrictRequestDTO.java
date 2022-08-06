53b9f485c387a3ba355ab465c5470e643b4c75eb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-06 09:18:39 +0700

    add constructor

--- src/main/java/com/ecommerce/dto/region/subDistrict/ListSubDistrictRequestDTO.java
+++ src/main/java/com/ecommerce/dto/region/subDistrict/ListSubDistrictRequestDTO.java
@@ -3,10 +3,12 @@ package com.ecommerce.dto.region.subDistrict;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ListSubDistrictRequestDTO {
 
