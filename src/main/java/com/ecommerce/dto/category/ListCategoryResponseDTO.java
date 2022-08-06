8bcb2aa9675e392a11447987e5ed5a477921a921
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-06 13:29:17 +0700

    add constructor

--- src/main/java/com/ecommerce/dto/category/ListCategoryResponseDTO.java
+++ src/main/java/com/ecommerce/dto/category/ListCategoryResponseDTO.java
@@ -1,11 +1,13 @@
 package com.ecommerce.dto.category;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ListCategoryResponseDTO {
 
