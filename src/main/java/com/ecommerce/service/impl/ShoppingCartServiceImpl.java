094693d2a786d458f2b43fd5c9fbb421d4cd21b7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-07 19:55:59 +0700

    update all method ShoppingCartServiceImpl

--- src/main/java/com/ecommerce/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/ShoppingCartServiceImpl.java
@@ -2,88 +2,107 @@ package com.ecommerce.service.impl;
 
 import com.ecommerce.entity.CartItem;
 import com.ecommerce.entity.ShoppingCart;
+import com.ecommerce.repository.CartItemRepository;
+import com.ecommerce.repository.ShoppingCartRepository;
 import com.ecommerce.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import com.ecommerce.service.ProductServiceNew;
+import com.ecommerce.service.ShoppingCartService;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Service
-@Slf4j
-@RequiredArgsConstructor
-public class ShoppingCartServiceImpl {
+public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     private final ShoppingCartRepository shoppingCartRepository;
     private final ProductService productService;
+    private final ProductServiceNew productServiceNew;
     private final CartItemRepository cartItemRepository;
 
-    public ShoppingCart addShoppingCartFirstTime(Long id, String sessionToken, int quantity) {
-        ShoppingCart shoppingCart = new ShoppingCart();
+    public ShoppingCartServiceImpl(ShoppingCartRepository shoppingCartRepository, ProductService productService, ProductServiceNew productServiceNew, CartItemRepository cartItemRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+        this.productService = productService;
+        this.productServiceNew = productServiceNew;
+        this.cartItemRepository = cartItemRepository;
+    }
+
+    @Override
+    public ShoppingCart addShoppingCartFirstTime(String productId, String sessionToken, int quantity) {
+        // create object CartItem (Keranjang tiap item product)
         CartItem cartItem = new CartItem();
         cartItem.setQuantity(quantity);
-        cartItem.setDate(new Date());
-        cartItem.setProduct(productService.getProductById(id));
+        cartItem.setDate(LocalDate.now());
+        cartItem.setProduct(productService.getProductId(productId));
+
+        // create object ShoppingCart
+        ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart.getItems().add(cartItem);
         shoppingCart.setSessionToken(sessionToken);
-        shoppingCart.setDate(new Date());
+        shoppingCart.setDate(LocalDate.now());
         return shoppingCartRepository.save(shoppingCart);
     }
 
-    public ShoppingCart addToExistingShoppingCart(Long id, String sessionToken, int quantity) {
-        ShoppingCart shoppingCart = shoppingCartRepository.findBySessionToken(sessionToken);
-        Product p = productService.getProductById(id);
-        Boolean productDoesExistInTheCart = false;
-        if (shoppingCart != null) {
-            Set<CartItem> items = shoppingCart.getItems();
-            for (CartItem item : items) {
-                if (item.getProduct().equals(p)) {
-                    productDoesExistInTheCart = true;
-                    item.setQuantity(item.getQuantity() + quantity);
-                    shoppingCart.setItems(items);
-                    return shoppingCartRepository.saveAndFlush(shoppingCart);
-                }
-
-            }
-        }
-        if (!productDoesExistInTheCart && (shoppingCart != null)) {
-            CartItem cartItem1 = new CartItem();
-            cartItem1.setDate(new Date());
-            cartItem1.setQuantity(quantity);
-            cartItem1.setProduct(p);
-            shoppingCart.getItems().add(cartItem1);
-            return shoppingCartRepository.saveAndFlush(shoppingCart);
-        }
-
-        return this.addShoppingCartFirstTime(id, sessionToken, quantity);
-
+    @Override
+    public ShoppingCart addToExistingShoppingCart(String shoppingCartId, String sessionToken, int quantity) {
+//        ShoppingCart shoppingCart = shoppingCartRepository.findBySessionToken(sessionToken);
+//        Product p = productService.getProductById(id);
+//        Boolean productDoesExistInTheCart = false;
+//        if (shoppingCart != null) {
+//            Set<CartItem> items = shoppingCart.getItems();
+//            for (CartItem item : items) {
+//                if (item.getProduct().equals(p)) {
+//                    productDoesExistInTheCart = true;
+//                    item.setQuantity(item.getQuantity() + quantity);
+//                    shoppingCart.setItems(items);
+//                    return shoppingCartRepository.saveAndFlush(shoppingCart);
+//                }
+//
+//            }
+//        }
+//        if (!productDoesExistInTheCart && (shoppingCart != null)) {
+//            CartItem cartItem1 = new CartItem();
+//            cartItem1.setDate(new Date());
+//            cartItem1.setQuantity(quantity);
+//            cartItem1.setProduct(p);
+//            shoppingCart.getItems().add(cartItem1);
+//            return shoppingCartRepository.saveAndFlush(shoppingCart);
+//        }
+//
+//        return this.addShoppingCartFirstTime(id, sessionToken, quantity);
+        return null;
     }
 
+    @Override
     public ShoppingCart getShoppingCartBySessionToken(String sessionToken) {
         return shoppingCartRepository.findBySessionToken(sessionToken);
     }
 
-    public CartItem updateShoppingCartItem(Long id, int quantity) {
-        CartItem cartItem = cartItemRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with " + id + " not found"));
-        cartItem.setQuantity(quantity);
-        return cartItemRepository.saveAndFlush(cartItem);
+    @Override
+    public CartItem updateShoppingCartItem(String shoppingCartId, int quantity) {
+//        CartItem cartItem = cartItemRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with " + id + " not found"));
+//        cartItem.setQuantity(quantity);
+//        return cartItemRepository.saveAndFlush(cartItem);
+        return null;
     }
 
-    public ShoppingCart removeCartIemFromShoppingCart(Long id, String sessionToken) {
-        ShoppingCart shoppingCart = shoppingCartRepository.findBySessionToken(sessionToken);
-        Set<CartItem> items = shoppingCart.getItems();
-        CartItem cartItem = null;
-        for (CartItem item : items) {
-            if (item.getId() == id) {
-                cartItem = item;
-            }
-        }
-        items.remove(cartItem);
-        cartItemRepository.delete(cartItem);
-        shoppingCart.setItems(items);
-        return shoppingCartRepository.save(shoppingCart);
+    @Override
+    public ShoppingCart removeCartIemFromShoppingCart(String shoppingCartId, String sessionToken) {
+//        ShoppingCart shoppingCart = shoppingCartRepository.findBySessionToken(sessionToken);
+//        Set<CartItem> items = shoppingCart.getItems();
+//        CartItem cartItem = null;
+//        for (CartItem item : items) {
+//            if (item.getId() == id) {
+//                cartItem = item;
+//            }
+//        }
+//        items.remove(cartItem);
+//        cartItemRepository.delete(cartItem);
+//        shoppingCart.setItems(items);
+//        return shoppingCartRepository.save(shoppingCart);
+        return null;
     }
 
+    @Override
     public void clearShoppingCart(String sessionToken) {
         ShoppingCart sh = shoppingCartRepository.findBySessionToken(sessionToken);
         shoppingCartRepository.delete(sh);
