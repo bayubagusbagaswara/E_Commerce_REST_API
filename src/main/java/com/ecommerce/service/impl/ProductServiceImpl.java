557d7cf478132abefb61e624c53439fc245cd5c3
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 09:31:35 +0700

    update ProductServiceImpl

--- src/main/java/com/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.service.impl;
 
+import com.ecommerce.entity.Coupon;
 import com.ecommerce.mapper.ProductMapper;
 import com.ecommerce.dto.product.*;
 import com.ecommerce.entity.Category;
@@ -17,9 +18,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.Base64;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -139,4 +150,122 @@ public class ProductServiceImpl implements ProductService {
         List<Product> productList = productRepository.findAllBySuppliersId(supplierId);
         return productMapper.fromProductList(productList);
     }
+
+    @Override
+    public Product getProductId(String productId) {
+        return productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException("id", productId));
+    }
+
+    @Override
+    public void addImageToProduct(MultipartFile file, String productId) {
+        //        Product p = productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with " + id + " not found"));
+//        Carousel carousel = new Carousel();
+//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+//        if (fileName.contains("..")) {
+//            System.out.println("not a valid file");
+//        }
+//        try {
+//            carousel.setImage(resizeImageForUse(Base64.getEncoder().encodeToString(file.getBytes()), 400, 400));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        p.getCarousel().add(carousel);
+//        productRepository.save(p);
+    }
+
+    @Override
+    public String resizeImageForUse(String src, int width, int height) {
+        BufferedImage image = base64ToBufferedImage(src);
+        image = resizeImage(image, width, height);
+        return bufferedImageToBase64(image);
+    }
+
+    @Override
+    public BufferedImage resizeImage(BufferedImage image, int width, int height) {
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        try {
+//            Thumbnails.of(image).size(width, height).outputFormat("JPEG").outputQuality(1).toOutputStream(outputStream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        byte[] data = outputStream.toByteArray();
+//        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+//        try {
+//            return ImageIO.read(inputStream);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        return null;
+    }
+
+    @Override
+    public BufferedImage base64ToBufferedImage(String base64Img) {
+        BufferedImage image = null;
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Img);
+
+        try {
+            image = ImageIO.read(new ByteArrayInputStream(decodedBytes));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    public String bufferedImageToBase64(BufferedImage image) {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(image, "JPEG", Base64.getEncoder().wrap(out));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString(StandardCharsets.ISO_8859_1);
+    }
+
+//    Coupon discount = couponRepository.findMax();
+//    List<Product> products = productRepository.findAll();
+//    Product featuredProduct = null;
+//        for (Product p : products) {
+//        if (p.getDiscount().equals(discount)) {
+//            featuredProduct = p;
+//            break;
+//        }
+//    }
+//        return featuredProduct;
+
+
+//    public void saveProductToDatabase(MultipartFile file, String name, String description, int quantity, Double price, String brand, String categories) {
+//
+//        // buat object Product
+//        Product product = new Product();
+//        // ambil nama file
+//        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+//        if (fileName.contains("..")) {
+//            System.out.println("not a valid file");
+//        }
+//
+//        // set image ke product
+//        try {
+//            product.setImage(resizeImageForUse(Base64.getEncoder().encodeToString(file.getBytes()), 400, 400));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // create product detail
+//        ProductDetail productDetail = new ProductDetail();
+//        productDetail.setDescription(description);
+//
+//        // create Coupon
+//        Coupon coupon = new Coupon();
+//        coupon.setDiscount(0);
+//
+//        product.setProductDetail(productDetail);
+//        product.setName(name);
+//        product.setQuantity(quantity);
+//
+//        // add categories to product
+//        product = addCategoriesToProduct(product, categories);
+//
+//        // save product
+//        productRepository.save(product);
 }
