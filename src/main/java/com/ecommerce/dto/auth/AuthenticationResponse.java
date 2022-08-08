5a0cc5644dad99b9447d2cecb86035839d7f62b9
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 10:59:05 +0700

    add dto AuthenticationResponse

---
+++ src/main/java/com/ecommerce/dto/auth/AuthenticationResponse.java
@@ -0,0 +1,25 @@
+package com.ecommerce.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private Instant expiresAt;
+
+    private final String tokenType = "Bearer";
+
+    private String username;
+}
