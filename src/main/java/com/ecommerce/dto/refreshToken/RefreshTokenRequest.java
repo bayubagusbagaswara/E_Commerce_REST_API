ae556ee248ee1b63855d7836e28d1cbe4320017e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 11:06:22 +0700

    add dto RefreshTokenRequest

---
+++ src/main/java/com/ecommerce/dto/refreshToken/RefreshTokenRequest.java
@@ -0,0 +1,19 @@
+package com.ecommerce.dto.refreshToken;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequest {
+
+    @NotBlank(message = "Refresh Token must not be blank")
+    private String refreshToken;
+
+}
