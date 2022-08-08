65451b477c5423a0d40504b59a38245f285aeb59
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 15:30:33 +0700

    add method contract logout

--- src/main/java/com/ecommerce/service/AuthService.java
+++ src/main/java/com/ecommerce/service/AuthService.java
@@ -2,6 +2,7 @@ package com.ecommerce.service;
 
 import com.ecommerce.dto.auth.AuthenticationResponse;
 import com.ecommerce.dto.auth.LoginRequest;
+import com.ecommerce.dto.auth.LogoutRequest;
 import com.ecommerce.dto.auth.RegisterRequest;
 import com.ecommerce.dto.refreshToken.RefreshTokenRequest;
 import com.ecommerce.dto.user.UserDTO;
@@ -13,4 +14,6 @@ public interface AuthService {
     AuthenticationResponse login(LoginRequest loginRequest);
 
     AuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
+
+    void logout(LogoutRequest logoutRequest);
 }
