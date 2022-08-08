5f303054b47414c7c1a847790d9fe7e2aa961b83
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 16:23:17 +0700

    add bean passwordEncoder

--- src/main/java/com/ecommerce/configuration/SecurityConfiguration.java
+++ src/main/java/com/ecommerce/configuration/SecurityConfiguration.java
@@ -1,4 +1,23 @@
 package com.ecommerce.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y);
+    }
+
 }
