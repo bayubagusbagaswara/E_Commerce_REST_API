a3418e77ea286ff88c6ef53b2283cd18f58d4f0e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 11:03:54 +0700

    add dto CreateUserRequest

---
+++ src/main/java/com/ecommerce/dto/user/CreateUserRequest.java
@@ -0,0 +1,37 @@
+package com.ecommerce.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateUserRequest {
+
+    @NotBlank(message = "Firstname must not be blank")
+    @Size(max = 40, message = "Firstname maximum length 40 characters")
+    private String firstName;
+
+    @NotBlank(message = "Lastname must not be blank")
+    @Size(max = 40, message = "Lastname maximum length 40 characters")
+    private String lastName;
+
+    @NotBlank(message = "Username must not be blank")
+    @Size(max = 15, message = "Username maximum length 15 characters")
+    private String username;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 6, max = 100, message = "Password minimum length 6 characters and maximum 100 characters")
+    private String password;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be formatted")
+    private String email;
+}
