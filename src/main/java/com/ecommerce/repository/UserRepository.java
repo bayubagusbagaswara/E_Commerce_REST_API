11b4ed523c28b299116a64b5256aac60c5ab3f67
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 16:18:08 +0700

    add UserRepository

---
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.user.User;
+import com.ecommerce.entity.user.UserPrincipal;
+import com.ecommerce.exception.ResourceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    default User getUser(UserPrincipal currentUser) {
+        return getUserByName(currentUser.getUsername());
+    }
+
+    default User getUserByName(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+    }
+
+}
