4996dccb48dac98b703797cd65748f42dd77fb8b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 11:01:29 +0700

    add dto RoleRequest

---
+++ src/main/java/com/ecommerce/dto/role/RoleRequest.java
@@ -0,0 +1,18 @@
+package com.ecommerce.dto.role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleRequest {
+
+    @NotBlank(message = "Role Name must not be blank")
+    private String name;
+}
