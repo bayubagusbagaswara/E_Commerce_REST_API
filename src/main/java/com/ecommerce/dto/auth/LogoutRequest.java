ff4a4a28fb7c3e75268705b85963954dbb972188
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 10:59:38 +0700

    add dto LogoutRequest

---
+++ src/main/java/com/ecommerce/dto/auth/LogoutRequest.java
@@ -0,0 +1,21 @@
+package com.ecommerce.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogoutRequest {
+
+    @NotBlank(message = "Username or Email must not be blank")
+    private String usernameOrEmail;
+
+    @NotBlank(message = "Refresh Token must not be blank")
+    private String refreshToken;
+}
