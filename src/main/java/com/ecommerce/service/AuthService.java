74d47bc0d508b1a135ae7b2de445200ab2ab3454
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 15:29:39 +0700

    add method contract refreshToken

--- src/main/java/com/ecommerce/service/AuthService.java
+++ src/main/java/com/ecommerce/service/AuthService.java
@@ -3,14 +3,14 @@ package com.ecommerce.service;
 import com.ecommerce.dto.auth.AuthenticationResponse;
 import com.ecommerce.dto.auth.LoginRequest;
 import com.ecommerce.dto.auth.RegisterRequest;
+import com.ecommerce.dto.refreshToken.RefreshTokenRequest;
 import com.ecommerce.dto.user.UserDTO;
 
 public interface AuthService {
 
     UserDTO registerNewUser(RegisterRequest registerRequest);
 
-    // login
     AuthenticationResponse login(LoginRequest loginRequest);
 
-    // logout
+    AuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
 }
