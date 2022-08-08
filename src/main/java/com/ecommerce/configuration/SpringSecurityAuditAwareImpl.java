4062ec8369f325fbb9b7f02c1e9ba1eaa56a11db
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 16:21:37 +0700

    add config auditing SpringSecurityAuditAwareImpl

---
+++ src/main/java/com/ecommerce/configuration/SpringSecurityAuditAwareImpl.java
@@ -0,0 +1,29 @@
+package com.ecommerce.configuration;
+
+import com.ecommerce.entity.user.UserPrincipal;
+import lombok.NonNull;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class SpringSecurityAuditAwareImpl implements AuditorAware<String> {
+
+    @NonNull
+    @Override
+    public Optional<String> getCurrentAuditor() {
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        UserPrincipal userPrincipal = (UserPrincipal) Objects.requireNonNull(authentication).getPrincipal();
+
+        return Optional.ofNullable(userPrincipal.getEmail());
+    }
+}
