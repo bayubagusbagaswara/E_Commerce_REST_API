54372d6c116aae4095da56fe6d8a85e3866686ae
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-08 15:33:27 +0700

    add service contract CustomUserDetailsService

---
+++ src/main/java/com/ecommerce/service/CustomUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.ecommerce.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface CustomUserDetailsService extends UserDetailsService {
+
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+    UserDetails loadUserById(String id);
+}
