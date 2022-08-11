f4194afdd40ebeec7e84c4ad12d87b2266d8aedc
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-11 18:26:15 +0700

    add concrete class AuthServiceImpl

---
+++ src/main/java/com/ecommerce/service/impl/AuthServiceImpl.java
@@ -0,0 +1,58 @@
+package com.ecommerce.service.impl;
+
+import com.ecommerce.dto.auth.AuthenticationResponse;
+import com.ecommerce.dto.auth.LoginRequest;
+import com.ecommerce.dto.auth.LogoutRequest;
+import com.ecommerce.dto.auth.RegisterRequest;
+import com.ecommerce.dto.refreshToken.RefreshTokenRequest;
+import com.ecommerce.dto.user.UserDTO;
+import com.ecommerce.jwt.JwtTokenProvider;
+import com.ecommerce.service.AuthService;
+import com.ecommerce.service.RefreshTokenService;
+import com.ecommerce.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+    private static final String USER_ROLE_NOT_SET = "User role not set";
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    @Autowired
+    public AuthServiceImpl(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, RefreshTokenService refreshTokenService, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.refreshTokenService = refreshTokenService;
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO registerNewUser(RegisterRequest registerRequest) {
+        return null;
+    }
+
+    @Override
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        return null;
+    }
+
+    @Override
+    public AuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        return null;
+    }
+
+    @Override
+    public void logout(LogoutRequest logoutRequest) {
+
+    }
+}
