fc82095e51033ce01469e0c9a62dc9ddc4f2e992
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-11 18:08:42 +0700

    update JwtAuthenticationEntryPoint

--- src/main/java/com/ecommerce/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ecommerce/jwt/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,32 @@
 package com.ecommerce.jwt;
 
-public class JwtAuthenticationEntryPoint {
+import com.ecommerce.dto.MessageResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
+
+        LOGGER.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        MessageResponse body = new MessageResponse(HttpStatus.UNAUTHORIZED.value(), "You need to login first in order to perform this action.");
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(httpServletResponse.getOutputStream(), body);
+    }
 }
