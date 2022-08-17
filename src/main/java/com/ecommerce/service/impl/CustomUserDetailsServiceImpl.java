9a1ce057e268e8268af9bb7c8ce6e02982c5b372
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-17 19:34:49 +0700

    update CustomUserDetailsServiceImpl

--- src/main/java/com/ecommerce/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/CustomUserDetailsServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ecommerce.service.impl;
 
 import com.ecommerce.entity.user.User;
-import com.ecommerce.entity.user.UserPrincipal;
+import com.ecommerce.entity.user.CustomUserDetails;
 import com.ecommerce.repository.UserRepository;
 import com.ecommerce.service.CustomUserDetailsService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
@@ -19,15 +24,29 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
 
     @Override
     public UserDetails loadUserById(String id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("User not found with id: %s", id)));
-        return UserPrincipal.createUser(user);
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException(String.format("User not found with id: %s", id)));
+        return mapFromUserToUserDetails(user);
     }
 
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("User not found with this username or email: %s", usernameOrEmail)));
-        return UserPrincipal.createUser(user);
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException(String.format("User not found with this username or email: %s", usernameOrEmail)));
+        return mapFromUserToUserDetails(user);
+    }
+
+    private CustomUserDetails mapFromUserToUserDetails(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return CustomUserDetails.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(user.getUserPassword().getPassword())
+                .authorities(authorities)
+                .build();
     }
 }
