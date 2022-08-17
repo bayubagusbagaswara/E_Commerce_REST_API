94691886fb4a173ef9aa7f50b56793099273398d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-17 19:47:50 +0700

    update method deleteUser

--- src/main/java/com/ecommerce/service/UserService.java
+++ src/main/java/com/ecommerce/service/UserService.java
@@ -21,6 +21,6 @@ public interface UserService {
 
     UserDTO updateUser(String userId, UpdateUserRequest updateUserRequest);
 
-    MessageResponse deleteUser(String userId);
+    void deleteUser(String userId);
 
 }
